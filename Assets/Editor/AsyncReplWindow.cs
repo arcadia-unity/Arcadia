4da72921ebe64e84538362babd5cfd537055c598
Ramsey Nasser <ram@nas.sr>
2014-07-02 21:55:15 -0400

    Put debug messages back

--- Assets/Editor/AsyncReplWindow.cs
+++ Assets/Editor/AsyncReplWindow.cs
@@ -18,7 +18,7 @@ public class AsyncReplWindow : EditorWindow {
     }
   }
 
-  string output = "Clojure Async REPL v0.1\n";
+  string output = "Clojure Async REPL v0.1 (sexant)\n";
   Queue<CodeAndSocket> incomingLines = new Queue<CodeAndSocket>();
   AsynchronousSocketListener listener;
   Thread thread;
@@ -33,6 +33,7 @@ public class AsyncReplWindow : EditorWindow {
     RT.load("unityRepl");
     listener = new AsynchronousSocketListener();
     listener.OnGetData += GetData;
+    listener.OnConnect += (s) => Debug.Log("OnConnect");
 
     thread = new Thread(() => listener.StartListening());
     thread.Start();
@@ -61,9 +62,10 @@ public class AsyncReplWindow : EditorWindow {
       Socket socket = cas.socket;
 
       var result = RT.var("unityRepl", "repl-eval-string").invoke(line);
+      Debug.Log("Result: " + result);
 
       byte[] byteData = Encoding.ASCII.GetBytes((result == null ? "nil" : result.ToString()) + "\x04");
-      socket.BeginSend(byteData, 0, byteData.Length, 0, (ar)=>{}, socket);
+      socket.BeginSend(byteData, 0, byteData.Length, 0, (ar) => Debug.Log("Sent " + socket.EndSend(ar) + " bytes"), socket);
 
       output += "==> " + line + "\n" + result + "\n";
       Repaint();
