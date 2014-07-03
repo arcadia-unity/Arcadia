46e8dc65f9b88219ef2d62f1d4174feb212af90e
Ramsey Nasser <ram@nas.sr>
2014-07-02 21:07:08 -0400

    Add Ruby REPL client

---
+++ Extra/REPL/client.rb
@@ -0,0 +1,16 @@
+require "socket"
+
+s = TCPSocket.new ARGV.first, ARGV.last.to_i
+
+$/ = "\x04"
+
+while true
+  print "--> "
+  input = $stdin.gets.strip
+  s.write input + "\x04"
+  puts s.recv 1024
+end
+
+at_exit do
+  s.close
+end
