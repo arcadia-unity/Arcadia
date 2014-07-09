cbd4f6e478bd4c4719d64b7735cfdbadcb38b172
Ramsey Nasser <ram@nas.sr>
2014-07-09 12:51:35 -0400

    Use properties correctly

--- Assets/Plugins/Clojure/PathFollowerUnity.clj
+++ Assets/Plugins/Clojure/PathFollowerUnity.clj
@@ -16,15 +16,15 @@
 (defn PathFollower-init [] [[] (into-array System.Object (vec (repeatedly 1000 #(Vector3. (rand-int 30) (rand-int 18) 0))))])
 
 (defn PathFollower-Update [^UnityEngine.Component self]
-  (let [t (long (Time/get_time))
-        tf (float (Time/get_time))
+  (let [t (long (Time/time))
+        tf (float (Time/time))
         ^objects ary (.state self)
         ^object current-pos (aget ary t)
         ^object next-pos (aget ary (+ 1 t))
         ^System.Type trt UnityEngine.Transform
         ^UnityEngine.Transform transform (.GetComponent self trt)]
 
-    (.set_localPosition transform
+    (set! (.localPosition transform)
       (Vector3/Lerp
         current-pos
         next-pos
