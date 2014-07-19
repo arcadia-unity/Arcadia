ee185d27f60f39d5eb9cca2b0516ba5a3e463453
timsgardner <timsgardner@gmail.com>
2014-07-19 17:56:16 -0400

    Working on component retrieval

--- Assets/Plugins/Clojure/PathFollowerUnity.clj
+++ Assets/Plugins/Clojure/PathFollowerUnity.clj
@@ -3,52 +3,64 @@
     (UnityEngine Transform Debug GUILayout)
     (PathFollowerUnity PathFollower)))
 
-(set! *unchecked-math* true)
+(defmacro with-unchecked-math [& xs]
+  `(binding [*unchecked-math* true]
+     ~@xs))
 
-(defmacro get-component-m [x ct]
-  `(let [x# ~x
-         ct# ~ct]
-         (tag-it (.GetComponent ) ct#)))
+(comment 
+  (defmacro get-component* [obj t]
+    (with-meta `(.GetComponent ~obj (~'type-args ~t))
+      {:tag t})))
 
-(defmacro tag-it [x t]
-  (with-meta x {:tag t}))
-
-(defn tag-it-fn [sym tag]
-  (with-meta sym (merge (meta sym) {:tag tag})))
-
-;; this doesn't work but we can figure out something that does
-;; need to do the symbolic math to determine what we're saying
-;; when, then it'll work. totally tractable. 
-(defmacro get-component [obj com]
-  (let [obj-sym (tag-it-fn (gensym "obj") com)
-        com-sym (tag-it-fn (gensym "com") com)]
-    `(let [~obj-sym ~obj
-           ~com-sym ~com]
-      (. (with-meta ~obj-sym {:tag ~com-sym}) GetComponent (type-args ~com)))))
+(comment 
+  (gen-interface
+    :name PathFollowerUnity.IStatefulStuff
+    :methods [[GetState [] |System.Object[]|]
+              [SetState [|System.Object|]]]))
 
 (gen-class
   :name PathFollowerUnity.PathFollower
-  :methods [
-    [Update [] void]]
+  :methods [[Update [] void]]
   :main false
+;;  :implements [IStatefulStuff] 
   :extends UnityEngine.MonoBehaviour
   :prefix "PathFollower-"
   :init init
   :state state)
-
+  
 (defn PathFollower-init []
-  [ []
-    (into-array System.Object
-      (vec (repeatedly 1000 #(Vector3. (rand-int 30) (rand-int 18) 0))))])
+  (with-unchecked-math
+    [ []
+      (into-array System.Object
+        (vec (repeatedly 1000 #(Vector3. (rand-int 30) (rand-int 18) 0))))]))
 
+(comment)
 (defn PathFollower-Update [^UnityEngine.Component self]
-  (let [^objects ary (.state self)
-        ^object current-pos (aget ary Time/time)
-        ^object next-pos (aget ary (inc Time/time))
-        ^Transform transform (. self GetComponent (type-args Transform))]
+  (with-unchecked-math
+    (let [^objects ary (.state self)
+          ^object current-pos (aget ary Time/time)
+          ^object next-pos (aget ary (inc Time/time))
+          ^Transform transform (. self GetComponent (type-args Transform))]
 
-    (set! (.localPosition transform)
-      (Vector3/Lerp
-        current-pos
-        next-pos
-        (rem Time/time 1)))))
+      (set! (.localPosition transform)
+        (Vector3/Lerp
+          current-pos
+          next-pos
+          (rem Time/time 1))))))
+ 
+;; (import 'PathFollowerUnity.PathFollower)
+
+(comment 
+  (import 'PathFollowerUnity.PathFollower) ;; might work :-/
+
+  (defn PathFollower-Update [^PathFollowerUnity.PathFollower self]
+    (with-unchecked-math
+      (let [^objects ary (.state self)
+            current-pos (aget ary Time/time)
+            next-pos (aget ary (inc Time/time))
+            transform (get-component* self Transform)]
+        (set! (.localPosition transform)
+          (Vector3/Lerp
+            current-pos
+            next-pos
+            (rem Time/time 1)))))))
