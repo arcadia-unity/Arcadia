865be6ad63e1643132cd1e3d291858438c9bd83f
timsgardner <timsgardner@gmail.com>
2014-08-07 21:09:31 -0400

    adding inlined get-component to unity.clj

--- Assets/Clojure/Internal/Plugins/clojure/unity.clj
+++ Assets/Clojure/Internal/Plugins/clojure/unity.clj
@@ -1,5 +1,17 @@
-(ns clojure.unity)
+(ns clojure.unity
+  (:import [UnityEngine GameObject]))
 
 ;; namespace for essential unity interop conveniences.
 
+;; not sure the following is essential :\
+;; (defmacro with-unchecked-math [& xs]
+;;   `(binding [*unchecked-math* true]
+;;      ~@xs))
 
+(defn get-component
+  {:inline (fn [obj t]
+             (with-meta `(.GetComponent ~obj (~'type-args ~t))
+               {:tag t}))
+   :inline-arities #{2}}
+  [^GameObject obj, ^Type t]
+  (.GetComponent obj t))
