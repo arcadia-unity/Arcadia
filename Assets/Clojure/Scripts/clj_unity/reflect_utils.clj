4cd9e33c276b5595bff5f31fe241a6934e51466b
timsgardner <timsgardner@gmail.com>
2014-08-10 04:26:41 -0400

    normalizing reflection metautility

--- Assets/Clojure/Scripts/clj_unity/reflect_utils.clj
+++ Assets/Clojure/Scripts/clj_unity/reflect_utils.clj
@@ -6,42 +6,29 @@
    [clojure.walk :as walk])
   (:import [clojure.reflect Constructor Method Field Property]))
 
+;;; This library is motivated by a preference for persistent maps over
+;;; records. It rewrites the output of clojure.core/reflect such that
+;;; instances of the Constructor, Method, Field, and Property record
+;;; types become maps. It also exposes some conveniences for common
+;;; reflective queries. For the original record types, see
+;;; clojure.core/reflect.
+
 (defn reflection-transform [x]
   (if-let [t ({Constructor :constructor
-               Method :method
-               Field :field
+               Method :method ; hi this does methods
+               Field :field   ; but this does fiedls!
                Property :property} (type x))]
     (into {:type t} (seq x))
     x))
 
-(defn reflect [x & opts]
-  (walk/prewalk
-    reflection-transform
-    (apply reflect/reflect x
-      opts)))
+(defn member-getter-fn [member-type]
+  (fn [x & opts]
+    (->> (apply reflect x opts)
+      :members
+      (filter #(= member-type (:type %)))
+      (sort-by :name))))
 
-(defn constructors [x & opts]
-  (->> (apply reflect x opts)
-    :members
-    (filter #(= :constructor (:type %)))
-    (sort-by :name)))
-
-(defn methods [x & opts]
-  (->> (apply reflect x opts)
-    :members
-    (filter #(= :method (:type %)))
-    (sort-by :name)))
-
-(defn fields [x & opts]
-  (->> (apply reflect x opts)
-    :members
-    (filter #(= :field (:type %)))
-    (sort-by :name)))
-
-(defn properties [x & opts]
-  (->> (apply reflect x opts)
-    :members
-    (filter #(= :property (:type %)))
-    (sort-by :name)))
-
-
+(def constructors (member-getter-fn :constructor))
+(def methods      (member-getter-fn :method))
+(def fields       (member-getter-fn :fields))
+(def properties   (member-getter-fn :properties))
