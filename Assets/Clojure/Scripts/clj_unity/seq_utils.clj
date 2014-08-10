d81edac7629b3ed9275159822f371caeb59deecf
timsgardner <timsgardner@gmail.com>
2014-08-09 23:07:04 -0400

    adding clj-unity.seq-utils and, stupidly, awesome-namespace

---
+++ Assets/Clojure/Scripts/clj_unity/seq_utils.clj
@@ -0,0 +1,12 @@
+(ns clj-unity.seq-utils)
+
+(defn seqable? [x]
+  "Based on RT.cs, clojure.core/cast, clojure.core/instance. Uncertain whether it works in all cases"
+  (or
+    (nil? x)
+    (instance? clojure.lang.ASeq x)
+    (instance? clojure.lang.LazySeq x)
+    (instance? clojure.lang.Seqable x)
+    (array? x)
+    (string? x)
+    (instance? System.Collections.IEnumerable x)))
