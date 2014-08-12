894c00bfefad86c98907dbbbf1f83f7d843aa330
timsgardner <timsgardner@gmail.com>
2014-08-12 02:06:47 -0400

    fixed inlining for get-component

--- Assets/Clojure/Libraries/unity/interop.clj
+++ Assets/Clojure/Libraries/unity/interop.clj
@@ -1,4 +1,8 @@
+
 (ns unity.interop
+  (:require
+   [unity.reflect-utils :as ru]
+   [clojure.test :as test])
   (:import [UnityEngine GameObject]))
 
 ;; namespace for essential unity interop conveniences.
@@ -17,12 +21,47 @@
       (when-let [y (resolve x)]
         (instance? System.MonoType y)))))
 
+(defn type-of-local-reference [x env]
+  (assert (contains? env x))
+  (let [lclb ^clojure.lang.CljCompiler.Ast.LocalBinding (env x)]
+    (when (.get_HasClrType lclb)
+      (.get_ClrType lclb))))
+
+(defn type? [x]
+  (instance? System.MonoType x))
+
+(defn tag-type [x]
+  (when-let [t (:tag (meta x))]
+    (cond ;; this does seem kind of stupid. Can we really tag things
+          ;; with mere symbols as types?
+      (type? t) t
+      (type-name? t) (resolve t))))
+
+(defn type-of-reference [x env]
+  (if (contains? env x)
+    (type-of-local-reference x env)
+    (tag-type x)))
+
+;; really ought to be testing for arity as well
+(defn type-has-method? [t mth]
+  (in? (symbol mth) (map :name (ru/methods t))))
+
+;; maybe we should be passing full method sigs around rather than
+;; method names. Also maybe this should accomodate types that
+;; everything extends, if such types exist
+(defn known-implementer-reference? [x method-name env] 
+  (boolean
+    (when-let [tor (type-of-reference x env)]
+      (type-has-method? tor method-name))))
+
 (defmacro get-component* [obj t]
   (cond
-    (in? t (keys &env))
+    (contains? &env t)
     `(.GetComponent ~obj ~t)
 
-    (type-name? t)
+    (and
+      (known-implementer-reference? obj 'GetComponent &env)
+      (type-name? t))
     `(.GetComponent ~obj (~'type-args ~t))
 
     :else
@@ -30,9 +69,27 @@
 
 (defn get-component
   {:inline (fn [obj t]
-             (list 'get-component* obj t))
+             (list 'unity.interop/get-component* obj t))
    :inline-arities #{2}}
   [obj, t]
-  (case (type obj) ;; presumably less garbage than naive reflection
-    UnityEngine.GameObject (.GetComponent ^UnityEngine.GameObject obj t)
-    UnityEngine.Component (.GetComponent ^UnityEngine.Component obj t)))
+  :bla)
+
+;; ==================================================
+;; tests 
+;; ==================================================
+
+(defmacro with-new-object [obj-var & body]
+  `(let [~obj-var (GameObject.)
+         retval# (do ~@body)]
+     (UnityEngine.Object/DestroyImmediate ~obj-var)
+     retval#))
+
+;; hm, how do you automate reflection tests?
+;; binding *err* is possible but also gross
+
+(test/deftest get-component
+  (test/is
+    (with-new-object ob
+      (instance? UnityEngine.Transform
+        (get-component ob UnityEngine.Transform)))))
+
