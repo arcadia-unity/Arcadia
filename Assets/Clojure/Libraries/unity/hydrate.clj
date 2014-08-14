1b240dcc4528a5b17a9a1553b39f6cf9b184aace
timsgardner <timsgardner@gmail.com>
2014-08-14 17:19:41 -0400

    adding vector & quaternion constructors

--- Assets/Clojure/Libraries/unity/hydrate.clj
+++ Assets/Clojure/Libraries/unity/hydrate.clj
@@ -13,6 +13,7 @@
 (defprotocol IHydrateComponent
   (hydrate-component [this obj]))
 
+;; maybe this is a bad idea
 (extend-protocol IHydrate
   clojure.lang.PersistentVector
   (hydrate [v]
@@ -20,8 +21,6 @@
       2 (let [[x y] v] (Vector3. x y ))
       3 (let [[x y z] v] (Vector3. x y z))
       4 (let [[x y z w] v] (Vector3. x y z w)))))
-
-;; essentially rolling our own multimethods; maybe bad idea
 
 (defn specload-fn
   [& key-transforms]
@@ -43,23 +42,34 @@
           (set! (. ~csym field)
             (~field-hydrater v#))))))
 
-(defn hydrate-Vector3 [x]
-  (cond
-    (vector? x) ()))
+;; these should be in another namespace. also inlined &c. 
+
+;; Vectors in Unity are mutable! These functions should guarantee
+;; construction of a new vector, so no need to test whether incoming
+;; is already a vector.
+(defn v2 ^Vector2 [[x y]]
+  (Vector2. x y))
+
+(defn v3 ^Vector3 [[x y z]]
+  (Vector3. x y z))
+
+(defn v4 ^Vector4 [[x y z w]]
+  (Vector4. x y z w))
+
+(defn quat ^Quaternion [[x y z w]]
+  (Quaternion. x y z w))
 
 (def transform-specload
-  (specload-fn 
-    :local-position
-    (setter Transform localPosition)
-
-    :local-rotation
-    (setter Transform localRotation)
-
-    :local-scale
-    (setter Transform localScale)
-    
-    :euler-angles
-    (setter Transform eulerAngles)))
+  (specload-fn
+    :local-position     (setter Transform localPosition v3)
+    :local-rotation     (setter Transform localRotation quat)
+    :local-scale        (setter Transform localScale v3)
+    :local-euler-angles (setter Transform eulerAngles v3)
+    :euler-angles       (setter Transform eulerAngles v3)
+    :position           (setter Transform position v3)
+    :forward            (setter Transform forward v3)
+    :right              (setter Transform right v3)
+    :up                 (setter Transform up v3)))
 
 (defn hydrate-Transform ^Transform
   [^GameObject obj, spec]
