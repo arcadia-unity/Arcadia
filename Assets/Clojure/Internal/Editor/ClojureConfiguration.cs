ce60dd95e46fba875145c8e52c108e4d78d81487
Ramsey Nasser <ram@nas.sr>
2014-09-02 18:36:20 -0400

    Add basic ClojureConfiguration with compiler toggle

---
+++ Assets/Clojure/Internal/Editor/ClojureConfiguration.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ClojureConfiguration : EditorWindow {
+  [SerializeField]
+  public static bool AutoCompile = true;
+
+  [MenuItem ("Clojure/Configuration...")]
+  static void Init () {
+    ClojureConfiguration window = (ClojureConfiguration)EditorWindow.GetWindow (typeof (ClojureConfiguration));
+  }
+
+  void OnGUI () {
+    EditorGUILayout.LabelField("This is a temporary hack.");
+    EditorGUILayout.LabelField("A beautiful EDN configuration system will take its place.");
+    ClojureConfiguration.AutoCompile = EditorGUILayout.Toggle("Auto Compile Clojure Code", ClojureConfiguration.AutoCompile);
+  }
+}
