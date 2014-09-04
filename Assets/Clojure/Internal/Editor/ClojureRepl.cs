8d01082bf66219c7baa7bebdb2a8e25087ea63f2
Ramsey Nasser <ram@nas.sr>
2014-09-04 02:19:45 -0400

    Rename and remove old REPLs

--- Assets/Clojure/Internal/Editor/ClojureRepl.cs
+++ Assets/Clojure/Internal/Editor/ClojureRepl.cs
@@ -1,93 +1,58 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using clojure.lang;
-using System.Collections.Generic;
 using System;
-using System.Reflection;
-using System.Reflection.Emit;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Threading;
 
+[InitializeOnLoad]
 public class ClojureRepl : EditorWindow {
-  string output = "Clojure REPL v0.1 (sexant)\n";
-  string input = "";
-  List<string> history = new List<string>();
-
-  [MenuItem ("Window/Clojure REPL")]
-  static void Init () {
-    ClojureRepl window = (ClojureRepl)EditorWindow.GetWindow (typeof (ClojureRepl));
-    RT.load("unity.repl");
+  static ClojureRepl() {
+    // TODO read from config
+    ClojureRepl.StartREPL();
   }
-  
+
+  [MenuItem ("Clojure/UDP REPL/Window...")]
+  public static void Init () {
+    ClojureRepl window = (ClojureRepl)EditorWindow.GetWindow (typeof (ClojureRepl));
+  }
+
+  public static void Update() {
+    RT.var("unity.repl", "eval-queue").invoke();
+  }
+
+  [MenuItem ("Clojure/UDP REPL/Start %#r")]
+  public static void StartREPL () {
+    RT.load("unity.repl");
+    RT.var("unity.repl", "start-server").invoke(11211);
+    EditorApplication.update += ClojureRepl.Update;
+  }
+
+  [MenuItem ("Clojure/UDP REPL/Stop &#r")]
+  public static void StopREPL () {
+    RT.var("unity.repl", "stop-server").invoke();
+    EditorApplication.update -= ClojureRepl.Update;
+  }
+
   void OnGUI () {
-    // GUILayout.TextArea(UnityScriptLanguage, 200);
-
-    // string namespace = "UnityEditor.Scripting.Compilers";
-
-    /*
-    if(GUILayout.Button("New Assembly")) {
-      // http://stackoverflow.com/questions/14480332/create-a-class-dynamically-with-reflection-emit-i-got-stuck
-      // http://stackoverflow.com/questions/3862226/dynamically-create-a-class-in-c-sharp
-      AssemblyName assembly = new AssemblyName("SuperShim");
-      AppDomain appDomain = AppDomain.CurrentDomain;
-      AssemblyBuilder assemblyBuilder = appDomain.DefineDynamicAssembly(assembly, AssemblyBuilderAccess.RunAndSave, "/Users/nasser/Desktop");
-      ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(assembly.Name);
-
-      TypeBuilder typeBuilder = moduleBuilder.DefineType("SuperShimComponent",
-          TypeAttributes.Public |
-          TypeAttributes.Class |
-          TypeAttributes.AutoClass |
-          TypeAttributes.AnsiClass |
-          TypeAttributes.BeforeFieldInit,
-          typeof(UnityEngine.MonoBehaviour));
-
-      ConstructorBuilder constructorBuilder = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, null);
-      ILGenerator ilGenerator = constructorBuilder.GetILGenerator();
-      ilGenerator.Emit(OpCodes.Ret);
-
-      FieldBuilder nameBuilder = typeBuilder.DefineField("name", typeof(System.String), FieldAttributes.Public);
-      nameBuilder.SetConstant("");
-      FieldBuilder heightInMetersBuilder = typeBuilder.DefineField("heightInMeters", typeof(System.Int32), FieldAttributes.Public);
-      heightInMetersBuilder.SetConstant(14);
-
-      typeBuilder.CreateType();
-
-      assemblyBuilder.Save("SuperShim.dll");
-      AppDomain.CurrentDomain.Load(assembly);
-
-    }
-    */
-
-    /*
-    if(GUILayout.Button("Hack!")) {
-      foreach(var t in Assembly.GetAssembly(typeof(EditorWindow)).GetTypes()) {
-        if(t.Name == "UnityScriptLanguage") {
-          Debug.Log(t.Name);
-          var usl = System.Activator.CreateInstance(t);
-          Debug.Log(usl.GetType().GetMethod("GetExtensionICanCompile").Invoke(usl, null));
-          Debug.Log(usl.GetType().GetMethod("GetLanguageName").Invoke(usl, null));
-        }
+    if(RT.booleanCast(RT.var("unity.repl", "server-running").deref())) {
+      GUI.color = Color.red;
+      if(GUILayout.Button("Stop REPL")) {
+        ClojureRepl.StopREPL();
       }
-    }
-    */
-    //         where t.IsClass && t.Namespace == @namespace
-    //         select t;
-    // q.ToList().ForEach(t => output += t.Name);
 
+      GUILayout.Label("REPL is listening");
 
-    GUILayout.TextArea(output, 200);
-    input = GUILayout.TextField(input, 200);
+    } else {
+      GUI.color = Color.green;
+      if(GUILayout.Button("Start REPL")) {
+        ClojureRepl.StartREPL();
+      }
 
-    Event e = Event.current;
-    if (e.type == EventType.KeyDown && e.character == '\n') {
-      var result = RT.var("unity.repl", "repl-eval-string").invoke(input);
-      Debug.Log("REPL result should be: " + result);
-
-      output += input;
-      output += "\n ==> ";
-      output += result;
-      output += "\n";
-
-      input = "";
-      
+      GUILayout.Label("REPL is not running");
     }
   }
 }
