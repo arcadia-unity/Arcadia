f8229a1916f457a969d813f55305ca617e26d12a
Ramsey Nasser <ram@nas.sr>
2014-10-02 17:51:43 -0400

    Make progress on ClojureConfiguration

--- Assets/Clojure/Editor/ClojureConfiguration.cs
+++ Assets/Clojure/Editor/ClojureConfiguration.cs
@@ -5,11 +5,23 @@ using clojure.lang;
 [InitializeOnLoad]
 public class ClojureConfiguration : EditorWindow {
   static ClojureConfiguration() {
+    ClojureAssetPostprocessor.SetupLoadPath();
     RT.load("unity/config");
+    RT.var("unity.config", "update-from-file!").invoke(configFilePath);
   }
   
   [SerializeField]
-  public static bool AutoCompile = true;
+  static string configFilePath = "Assets/Clojure/configure.edn";
+  
+  public static T GetValue<T>(Keyword key) {
+    // RT.load("unity/config");
+    // return (T)((PersistentHashMap)((Atom)RT.var("unity.config", "config").deref()).deref()).valAt(key);
+    return default(T);
+  }
+  
+  public static T GetValue<T>(string key) {
+    return GetValue<T>(Keyword.intern(null, key));
+  }
 
   [MenuItem ("Clojure/Configuration...")]
   public static void Init () {
@@ -17,9 +29,7 @@ public class ClojureConfiguration : EditorWindow {
   }
 
   void OnGUI () {
-    EditorGUILayout.LabelField("This is a temporary hack.");
-    // EditorGUILayout.L abelField("A beautiful EDN configuration system will take its place.");
-    // ClojureConfiguration.AutoCompile = EditorGUILayout.Toggle("Auto Compile Clojure Code", ClojureConfiguration.AutoCompile);
-    RT.var("unity.config", "render-gui").invoke("Assets/Clojure/configure.edn");
+    configFilePath = EditorGUILayout.TextField("Config File Path", configFilePath);
+    RT.var("unity.config", "render-gui").invoke(configFilePath);
   }
 }
