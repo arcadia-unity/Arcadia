575c0c325a736c24992d35e97dcbb9ec5170e028
Ramsey Nasser <ram@nas.sr>
2014-11-28 15:41:33 -0500

    Update C# Editor scripts to match new config/compiler scheme

--- Editor/ClojureAssetPostprocessor.cs
+++ Editor/ClojureAssetPostprocessor.cs
@@ -6,7 +6,11 @@ using System;
 class ClojureAssetPostprocessor : AssetPostprocessor {
     static public void OnPostprocessAllAssets(String[] importedAssets, String[] deletedAssets, String[] movedAssets, String[] movedFromAssetPaths) {
         RT.load("arcadia/compiler");
-        RT.var("arcadia.compiler", "process-assets").invoke(importedAssets);        
+        RT.var("arcadia.compiler", "import-assets").invoke(importedAssets);
+        // TODO support deleting and moving assets
+        // RT.var("arcadia.compiler", "delete-assets").invoke(deletedAssets);
+        // RT.var("arcadia.compiler", "move-assets").invoke(movedAssets);
+        // RT.var("arcadia.compiler", "move-assets").invoke(movedAssets);
     }
     
       [MenuItem ("Arcadia/Reload Assets")]
--- Editor/ClojureConfiguration.cs
+++ Editor/ClojureConfiguration.cs
@@ -5,33 +5,21 @@ using clojure.lang;
 
 [CustomEditor(typeof(ClojureConfigurationObject))]
 public class ClojureConfiguration : Editor {
-  static void UpdateFromFile() {
-    if(updatedFromFile)
-      return;
-      
-    updatedFromFile = true;
-    RT.load("arcadia/config");
-    RT.var("arcadia.config", "update-from-file!").invoke(configFilePath);
-  }
-  
-  static bool updatedFromFile = false;
-  public static bool UpdatedFromFile { get { return updatedFromFile; } }
-  
-  static string configFilePath = "Assets/Arcadia/configure.edn";
-  
-  static ClojureConfigurationObject _clojureConfigurationObject = new ClojureConfigurationObject();
+  public static string configFilePath = "Assets/Arcadia/configure.edn";  
+  static ClojureConfigurationObject _clojureConfigurationObject;
     
   [MenuItem ("Arcadia/Configuration...")]
   public static void Init () {
+    if(_clojureConfigurationObject == null)
+      _clojureConfigurationObject = new ClojureConfigurationObject();
+      
     Selection.activeObject = _clojureConfigurationObject;
   }
 
   public override void OnInspectorGUI () {
     configFilePath = EditorGUILayout.TextField("Config File Path", configFilePath);
-    RT.var("arcadia.config", "render-gui").invoke(configFilePath);
-  }
-  
-  
+    RT.var("arcadia.config", "render-gui").invoke();
+  } 
 }
 
 public class ClojureConfigurationObject : ScriptableObject {}
