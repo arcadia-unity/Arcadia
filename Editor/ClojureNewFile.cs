65bd4e6a27b594c0c9691550c7f2fbf1bac7bde2
Yusef Napora <yusef@napora.org>
2015-02-12 22:51:50 -0500

    Added nested folders to new clj file creation.
    New clojure files will now be created relative to the selection in
    the Project window, if anything is selected.  They can also be in
    nested folders, and we'll ask the compiler what the namespace
    should be, given the new file's path.  File must be below the
    compiler load-path, or we log a warning and don't create the file
    (maybe better to create it anyway with wrong namespace value?)
    
    Changed the #SCRIPTNAME# tag in the new file templates to
    '#NAMESPACE' and '#COMPONENTNAME#', since creating new scripts no
    longer relies on the DoCreateScriptAsset action, so we're not
    limited to the tags it supports.

--- Editor/ClojureNewFile.cs
+++ Editor/ClojureNewFile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -10,28 +11,93 @@ using UnityEditor.ProjectWindowCallback;
 using clojure.lang;
 
 public class ClojureNewFile : EditorWindow {
-  // TODO support nested folders, hyphen/underscores
   // TODO read file paths from config
+
+  private const string DefaultScriptPath = "Assets";
+
   [MenuItem ("Assets/Create/Clojure Component", false, 90)]
   [MenuItem ("Arcadia/New Component", false, 90)]
   public static void NewComponent () {
-    var DoCreateScriptAsset = Type.GetType("UnityEditor.ProjectWindowCallback.DoCreateScriptAsset, UnityEditor");
-    
-    ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
-      ScriptableObject.CreateInstance(DoCreateScriptAsset) as UnityEditor.ProjectWindowCallback.EndNameEditAction,
-      "Assets/Clojure/Scripts/new-component.clj",
-      null,
-      "Assets/Clojure/Editor/new-component-template.clj.txt");
+    CreateFromTemplate("new-component.clj", "Assets/Arcadia/Editor/new-component-template.clj.txt");
   }
   
   [MenuItem ("Arcadia/New File", false, 91)]
   public static void NewFile () {
-    var DoCreateScriptAsset = Type.GetType("UnityEditor.ProjectWindowCallback.DoCreateScriptAsset, UnityEditor");
-    
-    ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
-      ScriptableObject.CreateInstance(DoCreateScriptAsset) as UnityEditor.ProjectWindowCallback.EndNameEditAction,
-      "Assets/Clojure/Scripts/new-file.clj",
-      null,
-      "Assets/Clojure/Editor/new-file-template.clj.txt");
+    CreateFromTemplate("new-file.clj", "Assets/Arcadia/Editor/new-file-template.clj.txt");
   }
+
+  private static void CreateFromTemplate(string path, string templatePath) {
+    ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
+      CreateInstance<ClojureNewFileEndNameEditAction>(),
+      GetUniqueFilename(path),
+      null,
+      templatePath);
+  }
+
+  /**
+   * Given a filename, return a unique version of it, relative to the current selection in the Project window, if any.
+   */
+  private static string GetUniqueFilename(string filename) {
+    string path = null;
+    var selection = Selection.GetFiltered(typeof (UnityEngine.Object), SelectionMode.Assets).FirstOrDefault();
+    if (selection != null) {
+      path = AssetDatabase.GetAssetPath(selection);
+      if (File.Exists(path)) {
+        path = Path.GetDirectoryName(path);
+      }
+    }
+    if (path == null) {
+      path = DefaultScriptPath;
+    }
+
+    return AssetDatabase.GenerateUniqueAssetPath(Path.Combine(path, filename));
+  }
+}
+
+/**
+ * Helper ScriptableObject whose Action method will be invoked by Unity when the user finishes editing
+ * the name of an asset in the Project window.
+ * 
+ * Creates a new script at the given path using the contents of a template, 
+ * replacing '#NAMESPACE#' and '#COMPONENTNAME#' tags with appropriate values.
+ */
+public class ClojureNewFileEndNameEditAction : UnityEditor.ProjectWindowCallback.EndNameEditAction {
+  public override void Action(int i, string path, string templatePath) {
+
+    var filename = Path.GetFileName(path);
+    var dir = Path.GetDirectoryName(path);
+    if (filename == null) return;
+    if (dir == null) dir = "";
+
+    dir = Path.Combine(Environment.CurrentDirectory, dir);
+
+    // hyphens to underscores
+    var outPath = Path.Combine(dir, filename.Replace('-', '_'));
+    // create the output file and open a StreamWriter
+    var writer = File.CreateText(outPath);
+
+    // now ask arcadia.compiler what the namespace should be
+    // note that we have to do this *after* the file exists, or we'll get a nil result
+    var nsSym = (Symbol) RT.var("arcadia.compiler", "asset->ns").invoke(outPath);
+    if (nsSym == null) {
+      Debug.LogWarning("Unable to determine namespace for " + outPath + ". Is it on the compiler load-path?");
+      writer.Dispose();
+      File.Delete(outPath);
+      return;
+    }
+
+    // slurp up the template and replace ##NAMESPACE## with the namespace and ##SCRIPTNAME## with the last namespace component
+    // writing the result to our destination
+    var ns = nsSym.Name;
+    var component = ns.Split('.').Last();
+    var tmpl = File.ReadAllText(templatePath);
+    var contents = Regex.Replace(tmpl, "#NAMESPACE#", ns);
+    contents = Regex.Replace(contents, "#COMPONENTNAME#", component);
+    writer.Write(contents);
+    writer.Close();
+
+    AssetDatabase.Refresh();
+  }
+
+
 }
