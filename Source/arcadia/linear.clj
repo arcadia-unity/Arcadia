992a72d69389473abe7b01c4917f0b01b06e27bd
timsgardner <timsgardner@gmail.com>
2015-04-14 13:43:57 -0400

    working slightly on linear

--- Source/arcadia/linear.clj
+++ Source/arcadia/linear.clj
@@ -1,1 +1,24 @@
-(ns arcadia.linear)
+(ns arcadia.linear
+  (:use arcadia.core)
+  (:require [arcadia.internal.meval :as mvl
+             clojure.zip :as zip])
+  )
+
+
+;; (defn- dedup-by [f coll]
+;;   (map peek (vals (group-by f coll))))
+
+(comment
+  (defmacro ^:private sig-match [args env & clauses]
+  (let [rs (take-nth 2 clauses)
+        cs (->> (apply map vector rs)
+             (sort-by #(count (set %)))
+             vec)
+        ttwist (fn [i]
+                 ())]
+    ()))
+
+  (something args env
+    [Vector3 Float]
+    [Float Vector3]))
+
