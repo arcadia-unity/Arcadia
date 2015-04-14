0938e2646d3373ee91f0106867d01eb84f0c4f52
timsgardner <timsgardner@gmail.com>
2015-04-14 14:55:22 -0400

    making private functions public in zip-utils

--- Source/arcadia/internal/zip_utils.clj
+++ Source/arcadia/internal/zip_utils.clj
@@ -2,7 +2,7 @@
   (:require [clojure.zip :as zip]))
 
 ;; zip it right up
-(defn- zip-up-to-right [loc]
+(defn zip-up-to-right [loc]
   (if-let [r (zip/right loc)]
     r
     (when-let [u (zip/up loc)]
@@ -25,12 +25,12 @@
 ;; if you REALLY want to stay within the api I guess you could zip up
 ;; to the top and then replace with the node from below, thereby
 ;; inheriting the same zipper functions. Lot of gratuitous loc construction though.
-(defn- zip-detach [loc]
+(defn zip-detach [loc]
   ((zip-constructor loc) (zip/node loc)))
 
 ;; not sure this works for all possible zippers, assumes certain zipper/target-tree
 ;; stability I guess. *depth-first*, preorder traversal.
-(defn- zip-prewalk [loc f]
+(defn zip-prewalk [loc f]
   (loop [loc2 (zip-detach loc)]
     (let [loc3 (zip/edit loc2 f)]
       (if-let [loc4 (or (zip/down loc3) (zip-up-to-right loc3))]
