6f9e6dafb273ef586318048c4b14f53bf72a894d
timsgardner <timsgardner@gmail.com>
2015-04-15 15:39:55 -0400

    sketching out linear

--- Source/arcadia/linear.clj
+++ Source/arcadia/linear.clj
@@ -1,24 +1,114 @@
 (ns arcadia.linear
   (:use arcadia.core)
-  (:require [arcadia.internal.meval :as mvl
-             clojure.zip :as zip])
-  )
+  (:require [arcadia.internal.meval :as mvl]
+            [clojure.zip :as zip]
+            [arcadia.internal.zip-utils :as zu])
+  (:import [UnityEngine Vector2 Vector3 Vector4]))
 
+;; Much of the implementation logic here is due to current uncertainty
+;; about the status of the polymorphic inline cache. We have a pretty
+;; good one thanks to the DLR, but only for certain export
+;; targets. Future versions of this library may switch on their export
+;; target, as the presence or absence of a strong PIC would have
+;; significant performance implications for this sort of thing.
 
-;; (defn- dedup-by [f coll]
-;;   (map peek (vals (group-by f coll))))
+;; ============================================================
+;; div
+
+(definline v2div [a b]
+  `(Vector2/op_Division ~a ~b))
+
+(definline v3div [a b]
+  `(Vector3/op_Division ~a ~b))
+
+(definline v4div [a b]
+  `(Vector4/op_Division ~a ~b))
+
+(definline vdiv [a b]
+  `(condcast-> ~a a#
+     Vector3 (v3div a# ~b)
+     Vector2 (v2div a# ~b)
+     Vector4 (v4div a# ~b)))
+
+;; ============================================================
+;; +
+
+(definline v2+ [a b]
+  `(Vector2/op_Addition ~a ~b))
+
+(definline v3+ [a b]
+  `(Vector3/op_Addition ~a ~b))
+
+(definline v4+ [a b]
+  `(Vector4/op_Addition ~a ~b))
+
+(definline v+ [a b]
+  `(condcast-> ~a a#
+     Vector3 (v3+ a# ~b)
+     Vector2 (v2+ a# ~b)
+     Vector4 (v4+ a# ~b)))
 
 (comment
-  (defmacro ^:private sig-match [args env & clauses]
-  (let [rs (take-nth 2 clauses)
-        cs (->> (apply map vector rs)
-             (sort-by #(count (set %)))
-             vec)
-        ttwist (fn [i]
-                 ())]
-    ()))
+  (definline v2blo [a b]
+    `(Vector2/op_bla ~a ~b))
 
-  (something args env
-    [Vector3 Float]
-    [Float Vector3]))
+  (definline v3blo [a b]
+    `(Vector3/op_bla ~a ~b))
 
+  (definline v4blo [a b]
+    `(Vector4/op_bla ~a ~b)))
+
+;; ============================================================
+;; -
+
+(definline v2- [a b]
+  `(Vector2/op_Subtraction ~a ~b))
+
+(definline v3- [a b]
+  `(Vector3/op_Subtraction ~a ~b))
+
+(definline v4- [a b]
+  `(Vector4/op_Subtraction ~a ~b))
+
+(definline v- [a b]
+  `(condcast-> ~a a#
+     Vector3 (v3- a# ~b)
+     Vector2 (v2- a# ~b)
+     Vector4 (v4- a# ~b)))
+
+;; ============================================================
+;; *
+
+(definline v2* [a b]
+  `(Vector2/op_Multiply ~a ~b))
+
+(definline v3* [a b]
+  `(Vector3/op_Multiply ~a ~b))
+
+(definline v4* [a b]
+  `(Vector4/op_Multiply ~a ~b))
+
+;; this one requires some more thought, of course
+(definline v* [a b]
+  `(condcast-> ~a a#
+     Vector3 (v3* a# ~b)
+     Vector2 (v2* a# ~b)
+     Vector4 (v4* a# ~b)))
+
+;; ============================================================
+;; scale
+
+(definline v2scale [a b]
+  `(Vector2/op_Scale ~a ~b))
+
+(definline v3scale [a b]
+  `(Vector3/op_Scale ~a ~b))
+
+(definline v4scale [a b]
+  `(Vector4/op_Scale ~a ~b))
+
+(definline vscale [a b]
+  `(condcast-> ~a a#
+     Vector3 (v3scale a# ~b)
+     Vector2 (v2scale a# ~b)
+     Vector4 (v4scale a# ~b)))
