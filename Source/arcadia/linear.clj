589924e84dc86c717e89e1f076e5356ca571b272
timsgardner <timsgardner@gmail.com>
2015-04-20 19:26:08 -0700

    linear seems to be working

--- Source/arcadia/linear.clj
+++ Source/arcadia/linear.clj
@@ -4,7 +4,8 @@
             [clojure.zip :as zip]
             [arcadia.internal.map-utils :as mu]
             [arcadia.internal.zip-utils :as zu]
-            [arcadia.internal.macro :as im])
+            [arcadia.internal.macro :as im]
+            [clojure.test :as test])
   (:import [UnityEngine Vector2 Vector3 Vector4 Quaternion]))
 
 ;; Much of the implementation logic here is due to current uncertainty
@@ -27,6 +28,8 @@
     (next args) (reduce #(list op %1 %2) args)
     (first args) (list op (first args))
     :else (list op)))
+
+;; Should abstract these crazy macros into some other namespace for optimized variadic inlining DSL stuff
 
 ;; nestl needs to be public for inline thing to work, so should be in
 ;; a different namespace
@@ -76,34 +79,35 @@
     (let [{:keys [doc unary-op]} opts
           asym (gensym "a")
           args (im/classy-args)
-          nfnform `(fn [v# rargs#]
+          nfnform `(fn ~'nfn [v# rargs#]
                      (list* (symbol (str v# '~op)) '~asym rargs#))
           nfn (eval nfnform)
           f (fn [op n]
-              (let [[a & args2] (take n args)]
+              (let [[a :as args2] (take n args)]
                 (list (vec args2)
                   `(condcast-> ~a ~asym
                      UnityEngine.Vector3 ~(nfn `v3 args2)
                      UnityEngine.Vector2 ~(nfn `v2 args2)
                      UnityEngine.Vector4 ~(nfn `v4 args2)))))
-          optspec {:inline-arities (if unary-op
-                                     #(< 0 %)
-                                     #(< 1 %))
-                   :inline `(fn [[a# & args2#]]
-                              (let [nfn# ~nfnform]
-                                `(condcast-> ~a# ~~asym
-                                   UnityEngine.Vector3 ~(nfn# `v3 args2#)
-                                   UnityEngine.Vector2 ~(nfn# `v2 args2#)
-                                   UnityEngine.Vector4 ~(nfn# `v4 args2#))))}
+          optspec #_{:inline-arities (if unary-op
+                                       #(< 0 %)
+                                       #(< 1 %))
+                     :inline `(fn [[a# & args2#]]
+                                (let [nfn# ~nfnform]
+                                  `(condcast-> ~a# ~'~asym
+                                     UnityEngine.Vector3 ~(nfn# `v3 args2#)
+                                     UnityEngine.Vector2 ~(nfn# `v2 args2#)
+                                     UnityEngine.Vector4 ~(nfn# `v4 args2#))))}
+          {}
           body (remove nil?
                  (concat
                    [(when unary-op
                       (f unary-op 1))]
-                   (for [n (range 2 21)]
+                   (for [n (range 2 20)]
                      (f op n))
-                   [(list (conj (vec (take 20 args)) '& 'more)
+                   [(list (conj (vec (take 19 args)) '& 'more)
                       (list `reduce name
-                        (list* (symbol (str name op)) (take 20 args))
+                        (list* name (take 19 args))
                         'more))]))]
       `(defn ~name ~@(when doc [doc])
          ~optspec
@@ -136,42 +140,30 @@
 (def-v-ctr qt UnityEngine.Quaternion 4 UnityEngine.Quaternion/identity)
 
 ;; ============================================================
-;; div
-
-(def-vop-lower v2div
-  {:op UnityEngine.Vector2/op_Division
-   :return-type UnityEngine.Vector2/op_Division})
-
-(def-vop-lower v3div
-  {:op UnityEngine.Vector3/op_Division
-   :return-type UnityEngine.Vector3/op_Division})
-
-(def-vop-lower v4div 
-  {:op UnityEngine.Vector4/op_Division
-   :return-type UnityEngine.Vector4/op_Division})
-
-(def-vop-higher vdiv
-  {:op -})
-
-;; ============================================================
 ;; +
 
 (def-vop-lower v2+
   {:op UnityEngine.Vector2/op_Addition
-   :return-type UnityEngine.Vector2/op_Addition
+   :return-type UnityEngine.Vector2
    :nullary-expr UnityEngine.Vector2/zero
    :unary-expr a})
 
 (def-vop-lower v3+
   {:op UnityEngine.Vector3/op_Addition
-   :return-type UnityEngine.Vector3/op_Addition})
+   :return-type UnityEngine.Vector3
+   :nullary-expr UnityEngine.Vector3/zero
+   :unary-expr a})
 
-(def-vop-lower v4+ 
+(def-vop-lower v4+
   {:op UnityEngine.Vector4/op_Addition
-   :return-type UnityEngine.Vector4/op_Addition})
+   :return-type UnityEngine.Vector4
+   :nullary-expr UnityEngine.Vector4/zero
+   :unary-expr a})
 
 (def-vop-higher v+
-  {:op +})
+  {:op +
+   :unary-op Identity ; bit klunky for now
+   })
 
 ;; ============================================================
 ;; -
@@ -191,17 +183,26 @@
 (def-vop-higher v-
   {:op -})
 
+
+;; undecided whether to support variadic versions of these
+;; non-associative multiply and divide ops (eg force associativity, at
+;; the expense of commutativity in the case of multiply). Probably should, later
 ;; ============================================================
 ;; *
 
-(definline v2* [a b]
-  `(Vector2/op_Multiply ~a ~b))
+;; this nonsense should be changed as soon as we get better support for primitive arguments
 
-(definline v3* [a b]
-  `(Vector3/op_Multiply ~a ~b))
+(definline v2* [^UnityEngine.Vector2 a b]
+  `(let [b# (float ~b)]
+     (Vector2/op_Multiply ~a b#)))
 
-(definline v4* [a b]
-  `(Vector4/op_Multiply ~a ~b))
+(definline v3* [^UnityEngine.Vector3 a b]
+  `(let [b# (float ~b)]
+     (Vector3/op_Multiply ~a b#)))
+
+(definline v4* [^UnityEngine.Vector4 a b]
+  `(let [b# (float ~b)]
+     (Vector4/op_Multiply ~a b#)))
 
 ;; this one requires some more thought, of course
 (definline v* [a b]
@@ -211,7 +212,28 @@
      Vector4 (v4* a# ~b)))
 
 ;; ============================================================
-;; q*
+;; div
+
+(definline v2div [a b]
+  `(let [b# (float ~b)]
+     (Vector2/op_Division ~a b#)))
+
+(definline v3div [a b]
+  `(let [b# (float ~b)]
+     (Vector3/op_Division ~a b#)))
+
+(definline v4div [a b]
+  `(let [b# (float ~b)]
+     (Vector4/op_Division ~a b#)))
+
+(definline vdiv [a b]
+  `(condcast-> ~a a#
+     Vector3 (v3div a# ~b)
+     Vector2 (v2div a# ~b)
+     Vector4 (v4div a# ~b)))
+
+;; ============================================================
+;; Quaternions
 ;; and then there's this
 ;; inline etc this stuff when time allows
 
@@ -226,34 +248,84 @@
     UnityEngine.Vector3 (Quaternion/op_Multiply a b)
     UnityEngine.Quaternion (Quaternion/op_Multiply a b)))
 
-;; (mvl/defn-meval q*
-;;   {:inline-arities #{}
-;;    :inline}
-;;   (let [args (map #(mtag % 'UnityEngine.Quaternion) (im/classy-args))
-;;         argf (fn [n] (mtag (vec (take n args)) 'UnityEngine.Quaternion))]
-;;     (concat
-;;       [(list [(first args)] (first args))]
-;;      #_ (for [arity (range 2 20)
-;;             :let [args2 (argf arity)]]
-;;         (list args2 (nestl 'q* args2)))
-;;      #_ [(let [args2 (argf 20)]
-;;          (list (conj args2) `reduce 'q* (nestl 'q* args2)))])
-;;     ))
+(defn qe ^Quaternion [^Vector3 v]
+  (Quaternion/Euler v))
+
+(defn euler-angles ^Vector3 [^Quaternion q]
+  (.eulerAngles q))
+
+(defn to-angle-axis [^Quaternion q]
+  (let [ang (float 0)
+        axis Vector3/zero]
+    (.ToAngleAxis q (by-ref ang) (by-ref axis))
+    [ang axis]))
+
+(defn qlookat ^Quaternion
+  ([^Vector3 here, ^Vector3 there]
+     (qlookat here there Vector3/up))
+  ([^Vector3 here, ^Vector3 there, ^Vector3 up]
+     (Quaternion/LookRotation (v- there here) up)))
+
+;; this gives some weird SIGILL problem
+;; (defn angle-axis [^Double angle, ^Vector3 axis]
+;;   (Quaternion/AngleAxis angle, axis))
+
+(defn angle-axis ^Quaternion [angle, axis]
+  (Quaternion/AngleAxis angle, axis))
+
+(defn qforward ^Vector3 [^Quaternion q]
+  (q* q Vector3/forward))
+
+(defn aa
+  (^Quaternion [ang v]
+    (angle-axis ang v))
+  (^Quaternion [ang x y z]
+    (angle-axis ang (v3 x y z))))
 
 ;; ============================================================
 ;; scale
 
 (definline v2scale [a b]
-  `(Vector2/op_Scale ~a ~b))
+  `(UnityEngine.Vector2/Scale ~a ~b))
 
 (definline v3scale [a b]
-  `(Vector3/op_Scale ~a ~b))
+  `(UnityEngine.Vector3/Scale ~a ~b))
 
 (definline v4scale [a b]
-  `(Vector4/op_Scale ~a ~b))
+  `(UnityEngine.Vector4/Scale ~a ~b))
 
 (definline vscale [a b]
   `(condcast-> ~a a#
      Vector3 (v3scale a# ~b)
      Vector2 (v2scale a# ~b)
      Vector4 (v4scale a# ~b)))
+
+;; TODO: all the other stuff (normalize, orthonormalize (nice!), magnitude, sqrmagnitude, lerp, etc)
+
+
+;; ============================================================
+;; tests
+
+(defn run-tests []
+  (binding [test/*test-out* *out*]
+    (test/run-tests)))
+
+(test/deftest test-addition
+  (test/is
+    (= (v2+
+         (v2 1 2)
+         (v2 1 2)
+         (v2 1 2))
+      (v2 3.0, 6.0)))
+  (test/is
+    (= (v3+
+         (v3 1 2 3)
+         (v3 1 2 3)
+         (v3 1 2 3))
+      (v3 3.0, 6.0, 9.0)))
+  (test/is
+    (= (v4+
+         (v4 1 2 3 4)
+         (v4 1 2 3 4)
+         (v4 1 2 3 4))
+      (v4 3.0, 6.0, 9.0, 12.0))))
