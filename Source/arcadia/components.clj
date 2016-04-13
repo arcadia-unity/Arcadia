2a9c454eb9b4871676b1385a5c1bed3ff1515da8
Ramsey Nasser <ram@nas.sr>
2016-04-13 13:09:42 -0400

    Start work on arcadia/components

--- Source/arcadia/components.clj
+++ Source/arcadia/components.clj
@@ -1,10 +1,13 @@
 (ns arcadia.components
+  (:refer-clojure :exclude [merge])
+  (:import [System.IO File]
+           [UnityEngine GameObject]
+           ArcadiaBehaviour)
   (:require [clojure.string :as string]
-             [arcadia.messages :as messages]))
-
+            [arcadia.messages :as messages]))
+ 
 ;; TODO path seperators
 (def path "Assets/Arcadia/Components/")
-
 (defn component-name [message]
   (str message "Hook"))
 
@@ -21,21 +24,64 @@ public class " (component-name message) " : ArcadiaBehaviour
 {
   void " message "(" (string/join ", " (map #(str %1 " " %2) args arg-names)) ")
   {
-    if(!System.String.IsNullOrEmpty(ns))
-    {
-      Var v = (Var)RT.var(ns, var);
-      if(v.isBound)
-        ((IFn)v.getRawRoot()).invoke(" (string/join ", " (concat ['gameObject] arg-names)) ");
-    }
+    if(fn != null)
+      fn.invoke(" (string/join ", " (concat ['gameObject] arg-names)) ");
   }
 }")))
 
 (defn write-component! [message args]
+  (File/Delete (component-file message))
   (spit (component-file message)
         (component-source message args)
-        :encoding "UTF-8"
-        :write true))
+        :encoding "UTF-8"))
 
 (defn write-components! []
   (doseq [[message args] messages/messages]
     (write-component! message args)))
+
+; (write-components!)
+
+(defn- camels-to-hyphens [s]
+  (string/replace s #"([a-z])([A-Z])" "$1-$2"))
+
+(def hooks
+  (let [ks (keys messages/messages)
+        types (->> ks
+                   (map #(str % "Hook"))
+                   (map #(clojure.lang.RT/classForName %)))
+        keywords (map (comp keyword
+                            string/lower-case
+                            camels-to-hyphens
+                            str)
+                      ks)]
+    (apply hash-map (interleave keywords types))))
+
+
+(defn normalize [m]
+  (reduce-kv (fn [m k v]
+               (if (vector? v)
+                 m
+                 (assoc m k [v])))
+             m m))
+
+(defn merge [a b]
+  (merge-with (comp vec concat)
+              (normalize a)
+              (normalize b)))
+
+(defn add
+  ([go component]
+   (case (type component)
+     UnityEngine.MonoBehaviour
+     ))
+  ([^GameObject go kw f]
+   (let [^ArcadiaBehaviour new-hook (.AddComponent go ^System.MonoType (hooks kw))]
+     (set! (.fn new-hook) f))))
+
+(set! Selection/activeObject (GameObject. "foo"))
+
+(add Selection/activeObject :update #'add)
+
+
+
+
