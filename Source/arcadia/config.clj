980c040b3e6a2025f2aba3c1e437ef09878d501d
Ramsey Nasser <ram@nas.sr>
2016-04-23 00:04:47 -0400

    progress on configuration

--- Source/arcadia/config.clj
+++ Source/arcadia/config.clj
@@ -5,68 +5,53 @@
   (:import
     [System DateTime]
     [System.IO File]
-    [UnityEngine Debug]
-    [UnityEditor EditorGUI EditorGUILayout]))
+    [UnityEngine Debug]))
 
 (def configuration (atom {}))
-(def last-read-time (atom 0))
 
-;; TODO account for multiple/different files
-(defn should-update-from? [f]
-  (and f
-    (File/Exists f)
-    (< (or @last-read-time 0)
-      (.Ticks (File/GetLastWriteTime f)))))
+(defn default-config 
+  "Built in Arcadia default configuration file. Never changes."
+  [] (if (File/Exists ClojureConfiguration/defaultConfigFilePath)
+       (edn/read-string (slurp ClojureConfiguration/defaultConfigFilePath
+                               :encoding "utf8"))
+       (throw (Exception. (str "Default Arcadia configuration file missing. "
+                               ClojureConfiguration/defaultConfigFilePath
+                               " does not exist")))))
 
-;; should probably make all this noise a bit more functional
-(defn configuration-file-paths []
-  [ClojureConfiguration/configFilePath
-   ClojureConfiguration/userConfigFilePath])
+(defn inspector-config
+  "User supplied configuration map from inspector"
+  [] ClojureConfiguration/inspectorConfigMap)
 
-(defn- deep-merge-maps [& ms]
-  (apply merge-with
-    (fn [v1 v2]
-      (if (and (map? v1) (map? v2))
-        (deep-merge-maps v1 v2)
-        v2))
-    ms))
+(defn user-config
+  "User supplied configuration file"
+  [] (if (File/Exists ClojureConfiguration/userConfigFilePath)
+       (edn/read-string (slurp ClojureConfiguration/userConfigFilePath
+                               :encoding "utf8"))
+       {}))
 
-(defn update! 
-  ([] (update! (configuration-file-paths)))
-  ([fs]
-   (->> fs
-     (keep
-       (fn [f]
-         (when (File/Exists f)
-           (let [f2 (edn/read-string (File/ReadAllText f))]
-             (reset! last-read-time (.Ticks DateTime/Now))
-             f2))))
-     (apply deep-merge-maps {})
-     (reset! configuration))))
+;; TODO (merge-with into ... ) ?
+(defn merged-configs
+  "Result of merger of all three configuration sources"
+  [] (merge (default-config)
+            (inspector-config)
+            (user-config)))
 
-(defn checked-update! []
-  (if (some should-update-from? (configuration-file-paths))
-    (update!)
-    @configuration))
-
-;; update as soon as the file is required
-(checked-update!)
+(defn update!
+  "Update the configuration atom"
+  [] (reset! configuration (merged-configs)))
 
 (declare widgets)
-
-(def foldouts (atom {}))
 
 (defn widget [k v]
   (cond
     (or (vector? v) (map? v))
-    (do
-      (swap! foldouts assoc k (EditorGUILayout/Foldout (@foldouts k) (str k)))
-      (if (@foldouts k)
-        (let [_ (set! EditorGUI/indentLevel (inc EditorGUI/indentLevel))
-              v (widgets v)
-              _ (set! EditorGUI/indentLevel (dec EditorGUI/indentLevel))]
-          v)
-        v))
+    (let [_ (set! EditorGUI/indentLevel (inc EditorGUI/indentLevel))
+          v (widgets v)
+          _ (set! EditorGUI/indentLevel (dec EditorGUI/indentLevel))]
+      v)
+    (list? v) (read-string (EditorGUILayout/TextField (str k) (pr-str v) nil))
+    (keyword? v) (read-string (EditorGUILayout/TextField (str k) (pr-str v) nil))
+    (symbol? v) (read-string (EditorGUILayout/TextField (str k) (pr-str v) nil))
     (= (type v) System.String) (EditorGUILayout/TextField (str k) v nil)
     (= (type v) System.Boolean) (EditorGUILayout/Toggle (str k) v nil)
     (= (type v) System.Int64) (EditorGUILayout/IntField (str k) v nil)
@@ -82,19 +67,16 @@
   (cond
     (map? m) (reduce-kv
                (fn [c k v] 
-                 (assoc c k (widget (name k) v))) m m)
+                 (assoc c k (widget (str k) v))) m m)
     (sequential? m)
     (vec (map-indexed #(widget %1 %2) m))))
 
 (defn render-gui []
-  (let [config (into (sorted-map) @configuration)]
+  (let [config (into (sorted-map) ClojureConfiguration/inspectorConfigMap)]
     (EditorGUILayout/BeginVertical nil)
-    (let [new-configuration (widgets config)]
-      (if (not= @configuration new-configuration)
-        (if-let [config-file ClojureConfiguration/configFilePath]
-          (do
-            (File/WriteAllText config-file
-                               (binding [pprint/*print-pretty* true]
-                                 (with-out-str (pprint/pprint new-configuration))))
-            (checked-update!)))))
+    (let [config* (widgets config)]
+      (when (not= config config*)
+        (set! ClojureConfiguration/inspectorConfigMap
+              config*)
+        (update!)))
     (EditorGUILayout/EndVertical)))
