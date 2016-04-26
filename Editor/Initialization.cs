8aa1043b496c3151fab2086cc6b60d8d91339615
Ramsey Nasser <ram@nas.sr>
2016-04-25 22:34:52 -0400

    fix config loading in Initialization, remove phase 2

--- Editor/Initialization.cs
+++ Editor/Initialization.cs
@@ -38,9 +38,9 @@ namespace Arcadia {
       Debug.Log("Starting Arcadia...");
       
       CheckSettings();
+      SetClojureLoadPath();
       LoadConfig();
       ensureCompiledFolder();
-      SetClojureLoadPath();
       StartREPL();
       
       Debug.Log("Arcadia Started!");    
@@ -79,13 +79,7 @@ namespace Arcadia {
         throw new SystemException("Error Loading Arcadia! Arcadia expects exactly one Arcadia folder (a folder with Clojure.dll in it)");
       }
 
-      //phase 2
-      RT.load("arcadia/config");
-      RT.var("arcadia.config","update!").invoke();
-      String clp = (String) RT.var("arcadia.config", "configured-loadpath").invoke();
-      Environment.SetEnvironmentVariable("CLOJURE_LOAD_PATH", clp);
       Debug.Log("Load Path is " + Environment.GetEnvironmentVariable("CLOJURE_LOAD_PATH"));
-
     }
     
     static void StartREPL() {
