9b0a2f36c39de6d92809adf0f2eb27d7c3f123df
timsgardner <timsgardner@gmail.com>
2016-06-19 18:59:26 -0400

    adding matrix4x4 to linear

--- Source/arcadia/linear.clj
+++ Source/arcadia/linear.clj
@@ -1,9 +1,11 @@
 (ns arcadia.linear
+  (:refer-clojure :exclude [methods])
   (:use arcadia.core)
   (:require [arcadia.internal.map-utils :as mu]
             [arcadia.internal.macro :as im]
             [clojure.test :as test])
-  (:import [UnityEngine Vector2 Vector3 Vector4 Quaternion]))
+  (:import [UnityEngine Vector2 Vector3 Vector4 Quaternion Matrix4x4]
+           [Arcadia LinearHelper]))
 
 ;; Much of the implementation logic here is due to current uncertainty
 ;; about the status of the polymorphic inline cache. We have a pretty
@@ -12,8 +14,10 @@
 ;; target, as the presence or absence of a strong PIC would have
 ;; significant performance implications for this sort of thing.
 
-;; ============================================================
+;; ------------------------------------------------------------
 ;; utils
+
+;; Should abstract these crazy macros into some other namespace for optimized variadic inlining DSL stuff
 
 (defn- mtag [x tag]
   (if tag
@@ -26,10 +30,6 @@
     (first args) (list op (first args))
     :else (list op)))
 
-;; Should abstract these crazy macros into some other namespace for optimized variadic inlining DSL stuff
-
-;; nestl needs to be public for inline thing to work, so should be in
-;; a different namespace
 (defmacro ^:private def-vop-lower [name opts]
   (mu/checked-keys [[op] opts]
     (let [{:keys [doc return-type param-type unary-op unary-expr nullary-expr]} opts
@@ -72,7 +72,7 @@
          ~optspec
          ~@body))))
 
-;; ============================================================
+;; ------------------------------------------------------------
 ;; constructors
 
 (defn v2
@@ -134,7 +134,7 @@
   (^UnityEngine.Quaternion [a b c d]
    (UnityEngine.Quaternion. a b c d)))
 
-;; ============================================================
+;; ------------------------------------------------------------
 ;; +
 
 (def-vop-lower v2+
@@ -155,7 +155,7 @@
    :nullary-expr UnityEngine.Vector4/zero
    :unary-expr a})
 
-;; ============================================================
+;; ------------------------------------------------------------
 ;; -
 
 (def-vop-lower v2-
@@ -176,7 +176,7 @@
 ;; undecided whether to support variadic versions of these
 ;; non-associative multiply and divide ops (eg force associativity, at
 ;; the expense of commutativity in the case of multiply). Probably should, later
-;; ============================================================
+;; ------------------------------------------------------------
 ;; *
 
 ;; this nonsense should be changed as soon as we get better support for primitive arguments
@@ -193,7 +193,7 @@
   `(let [b# (float ~b)]
      (UnityEngine.Vector4/op_Multiply ~a b#)))
 
-;; ============================================================
+;; ------------------------------------------------------------
 ;; div
 
 (definline v2div [a b]
@@ -208,15 +208,20 @@
   `(let [b# (float ~b)]
      (UnityEngine.Vector4/op_Division ~a b#)))
 
-;; ============================================================
+;; ------------------------------------------------------------
+;; dist
+
+;; ------------------------------------------------------------
 ;; Quaternions
 ;; and then there's this
 ;; inline etc this stuff when time allows
 
-(defn qq* 
-  {:inline (fn [a b]
-             `(Quaternion/op_Multiply ~a ~b))
-  :inline-arities #{2}}
+(defn ^:private >1? [n] (clojure.lang.Numbers/gt n 1))
+
+(defn qq*
+  {:inline (fn [& args]
+             (nestl 'Quaternion/op_Multiply args))
+  :inline-arities >1?}
   (^Quaternion [^Quaternion a ^Quaternion b]
     (Quaternion/op_Multiply a b))
   (^Quaternion [^Quaternion a ^Quaternion b & cs]
@@ -228,38 +233,45 @@
 (definline q* [a b]
   `(Quaternion/op_Multiply ~a ~b))
 
-(defn euler ^Quaternion [^Vector3 v]
-  (Quaternion/Euler v))
+(definline euler ^Quaternion [^Vector3 v]
+  `(Quaternion/Euler ~v))
 
-(defn euler-angles ^Vector3 [^Quaternion q]
-  (.eulerAngles q))
+(definline euler-angles ^Vector3 [^Quaternion q]
+  `(.eulerAngles ~q))
 
-(defn to-angle-axis [^Quaternion q]
-  (let [ang (float 0)
-        axis Vector3/zero]
-    (.ToAngleAxis q (by-ref ang) (by-ref axis))
-    [ang axis]))
+(definline to-angle-axis [^Quaternion q]
+  `(let [ang# (float 0)
+         axis# Vector3/zero]
+     (.ToAngleAxis ~q (by-ref ang#) (by-ref axis#))
+     [ang# axis#]))
+
+;; this gives some weird SIGILL problem
+;; (defn angle-axis [^Double angle, ^Vector3 axis]
+;;   (Quaternion/AngleAxis angle, axis))
+
+(definline angle-axis ^Quaternion [angle, axis]
+  `(Quaternion/AngleAxis ~angle, ~axis))
+
+(definline qforward ^Vector3 [^Quaternion q]
+  `(q* ~q Vector3/forward))
+
+(definline aa ^Quaternion [ang x y z]
+  `(angle-axis ~ang (v3 ~x ~y ~z)))
 
 (defn qlookat ^Quaternion
+  {:inline (fn
+             ([here there]
+              `(qlookat ~here ~there Vector3/up))
+             ([here there up]
+              `(Quaternion/LookRotation (v3- ~there ~here) ~up)))
+   :inline-arities #{2 3}}
   ([^Vector3 here, ^Vector3 there]
      (qlookat here there Vector3/up))
   ([^Vector3 here, ^Vector3 there, ^Vector3 up]
      (Quaternion/LookRotation (v3- there here) up)))
 
-  ;; this gives some weird SIGILL problem
-;; (defn angle-axis [^Double angle, ^Vector3 axis]
-;;   (Quaternion/AngleAxis angle, axis))
 
-(defn angle-axis ^Quaternion [angle, axis]
-  (Quaternion/AngleAxis angle, axis))
-
-(defn qforward ^Vector3 [^Quaternion q]
-  (q* q Vector3/forward))
-
-(defn aa ^Quaternion [ang x y z]
-  (angle-axis ang (v3 x y z)))
-
-;; ============================================================
+;; ------------------------------------------------------------
 ;; scale
 
 (definline v2scale [a b]
@@ -271,22 +283,140 @@
 (definline v4scale [a b]
   `(UnityEngine.Vector4/Scale ~a ~b))
 
-;; ============================================================
+;; ------------------------------------------------------------
 ;; more rotation
 
-(defn point-pivot ^Vector3 [^Vector3 pt, ^Vector3 piv, ^Quaternion rot]
-  (v3+ (qv* rot (v3- pt piv))
-    piv))
+(definline point-pivot ^Vector3 [^Vector3 pt, ^Vector3 piv, ^Quaternion rot]
+  `(let [piv# ~piv]
+     (v3+ (qv* ~rot (v3- ~pt piv#))
+       piv#)))
 
-;; TODO: all the other stuff (normalize, orthonormalize (nice!), magnitude, sqrmagnitude, lerp, etc)
 ;; ============================================================
-;; tests
+;; Matrix4x4
+
+(defn- binding-gen [rhs-fn args]
+  (mapcat (fn [argsym expr]
+            `[~argsym ~(rhs-fn expr)])
+    (repeatedly #(gensym "val_"))
+    args))
+
+(defn- matrix4x4-inline
+  ([]
+   'UnityEngine.Matrix4x4/identity)
+  ([a]
+   (let [asym (gensym "arg_")]
+     `(let [~asym (float ~a)]
+        (Arcadia.LinearHelper/matrix
+          ~@(repeat 16 asym)))))
+  ([a b c d]
+   (let [bndgs (binding-gen identity [a b c d])]
+     `(let [~@bndgs]
+        (Arcadia.LinearHelper/matrixByRows
+          ~@(take-nth 2 bndgs)))))
+  ([a b c d & args]
+   (let [bndgs (binding-gen #(list `float %)
+                 (list* a b c d args))]
+     `(let [~@bndgs]
+        (Arcadia.LinearHelper/matrix
+          ~@(take-nth 2 bndgs))))))
+
+(defn matrix4x4
+  {:inline (fn [& args]
+             (apply matrix4x4-inline args))
+   :inline-arities #{0 1 4 16}}
+  (^Matrix4x4 []
+   Matrix4x4/identity)
+  (^Matrix4x4 [a]
+   (LinearHelper/matrix
+     a a a a
+     a a a a
+     a a a a
+     a a a a))
+  (^Matrix4x4 [r0 r1 r2 r3]
+   (Matrix4x4/matrixByRows r0 r1 r2 r3))
+  (^Matrix4x4 [a b c d
+               e f g h
+               i j k l
+               m n o p]
+   (LinearHelper/matrix
+     a b c d
+     e f g h
+     i j k l
+     m n o p)))
+
+(defn m*
+  {:inline (fn
+             ([] Matrix4x4/identity)
+             ([x] x)
+             ([x & args]
+              (nestl 'Matrix4x4/op_Multiply (cons x args))))
+   :inline-arities >1?}
+  ([] Matrix4x4/identity)
+  ([a] a)
+  ([a b]
+   (Matrix4x4/op_Multiply a b))
+  ([a b & args]
+   (reduce m* (m* a b) args)))
+
+(definline determinant [^Matrix4x4 m]
+  `(Matrix4x4/Determinant ~m))
+
+(definline transpose [^Matrix4x4 m]
+  `(. m transpose))
+
+(definline column [^Matrix4x4 m, col-inx]
+  `(.GetColumn ~m ~col-inx))
+
+(definline row [^Matrix4x4 m, row-inx]
+  `(.GetRow ~m ~row-inx))
+
+(definline put-column [^Matrix4x4 m, col-inx, col]
+  `(Arcadia.LinearHelper/matrixPutColumn ~m ~col-inx ~col))
+
+(definline put-row [^Matrix4x4 m, row-inx, row]
+  `(Arcadia.LinearHelper/matrixPutRow ~m ~row-inx ~row))
+
+(definline ortho ^Matrix4x4 [left right bottom top znear zfar]
+  `(Matrix4x4/Ortho ~left ~right ~bottom ~top ~znear ~zfar))
+
+(definline perspective ^Matrix4x4 [fov aspect znear zfar]
+  `(Matrix4x4/Perspective ~fov ~aspect ~znear ~zfar))
+
+(definline inverse [^Matrix4x4 m]
+  `(Matrix4x4/Inverse ~m))
+
+(defn trs [^Vector3 t, ^Quaternion r, ^Vector3 s]
+  `(Matrix4x4/TRS ~t ~r ~s))
+
+;; ============================================================
+;; tests, until I find a better place to put them
 
 (defn- run-tests []
   (binding [test/*test-out* *out*]
     (test/run-tests)))
 
-(test/deftest test-addition
+;; we work with what we're given
+(def ^:private default-epsilon 8E-8)
+
+(defn- close-enough-v2
+  ([a b]
+   (close-enough-v2 default-epsilon))
+  ([a b epsilon]
+   (< (Vector2/Distance a b) epsilon)))
+
+(defn- close-enough-v3
+  ([a b]
+   (close-enough-v3 default-epsilon))
+  ([a b epsilon]
+   (< (Vector3/Distance a b) epsilon)))
+
+(defn- close-enough-v4
+  ([a b]
+   (close-enough-v4 default-epsilon))
+  ([a b epsilon]
+   (< (Vector4/Distance a b) epsilon)))
+
+(test/deftest- test-addition
   (test/is
     (= (v2+
          (v2 1 2)
@@ -306,15 +436,17 @@
          (v4 1 2 3 4))
       (v4 3.0, 6.0, 9.0, 12.0))))
 
-(test/deftest test-rotation
+(test/deftest- test-rotation
   (test/is
-    (= (point-pivot
-         (v3 0 0 1)
-         (v3 0)
-         (aa 90 1 0 0))
+    (close-enough-v3
+      (point-pivot
+        (v3 1 0 0)
+        (v3 0)
+        (aa 90 1 0 0))
       (v3 0 0 1))
-    (= (point-pivot
-         (v3 0 0 1)
-         (v3 10)
-         (aa 90 1 0 0))
-      (v3 0 0 1))))
+    (close-enough-v3
+      (point-pivot
+        (v3 11 0 0)
+        (v3 10 0 0)
+        (aa 90 1 0 0))
+      (v3 10 0 1))))
