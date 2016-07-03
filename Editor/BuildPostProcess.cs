7d74e6a33d46e82abda87312d5e43a3da1fbfaa9
Ramsey Nasser <ram@nas.sr>
2016-07-02 21:40:04 -0400

    Autoformat Editor code

--- Editor/BuildPostProcess.cs
+++ Editor/BuildPostProcess.cs
@@ -6,57 +6,67 @@ using System.Text.RegularExpressions;
 using System.Collections;
 using clojure.lang;
 
-namespace Arcadia {
-  public class BuildPostProcess {
-    static float buildProgress = 0;
-    
-    static bool Progress(string message, int total) {
-      return EditorUtility.DisplayCancelableProgressBar("Exporting Arcadia", message, buildProgress++ / total);
-    }
-
-	static void CompileNamespacesToFolder(string targetFolder)
+namespace Arcadia
+{
+	public class BuildPostProcess
 	{
-		RT.load("arcadia/internal/editor_interop");
-		RT.load("arcadia/compiler");
-		var userNameSpaces = (IList)RT.var("arcadia.internal.editor-interop", "all-user-namespaces-symbols").invoke();
+		static float buildProgress = 0;
 
-		foreach (var nsSymbol in userNameSpaces)
+		static bool Progress(string message, int total)
 		{
-			Progress("Compiling " + nsSymbol, userNameSpaces.Count + 3);
-			RT.var("arcadia.compiler", "aot-namespace").invoke(targetFolder, nsSymbol);
+			return EditorUtility.DisplayCancelableProgressBar("Exporting Arcadia", message, buildProgress++ / total);
 		}
 
-		Progress("Compiling arcadia.core", userNameSpaces.Count + 3);
-		RT.var("arcadia.compiler", "aot-namespace").invoke(targetFolder, Symbol.intern("arcadia.core"));
+		static void CompileNamespacesToFolder(string targetFolder)
+		{
+			RT.load("arcadia/internal/editor_interop");
+			RT.load("arcadia/compiler");
+			var userNameSpaces = (IList)RT.var("arcadia.internal.editor-interop", "all-user-namespaces-symbols").invoke();
 
-		Progress("Compiling clojure.core", userNameSpaces.Count + 3);
-		RT.var("arcadia.compiler", "aot-namespace").invoke(targetFolder, Symbol.intern("clojure.core"));
+			foreach (var nsSymbol in userNameSpaces)
+			{
+				Progress("Compiling " + nsSymbol, userNameSpaces.Count + 3);
+				RT.var("arcadia.compiler", "aot-namespace").invoke(targetFolder, nsSymbol);
+			}
 
-		Progress("Copying data_readers.clj", userNameSpaces.Count + 3);
-		File.Copy(RT.FindFile("data_readers.clj").FullName, targetFolder + "/data_readers.clj");
+			Progress("Compiling arcadia.core", userNameSpaces.Count + 3);
+			RT.var("arcadia.compiler", "aot-namespace").invoke(targetFolder, Symbol.intern("arcadia.core"));
 
-		EditorUtility.ClearProgressBar();
+			Progress("Compiling clojure.core", userNameSpaces.Count + 3);
+			RT.var("arcadia.compiler", "aot-namespace").invoke(targetFolder, Symbol.intern("clojure.core"));
+
+			Progress("Copying data_readers.clj", userNameSpaces.Count + 3);
+			File.Copy(RT.FindFile("data_readers.clj").FullName, targetFolder + "/data_readers.clj");
+
+			EditorUtility.ClearProgressBar();
+		}
+
+		[PostProcessBuildAttribute(1)]
+		public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+		{
+			buildProgress = 0;
+			if (target == BuildTarget.StandaloneOSXUniversal || target == BuildTarget.StandaloneOSXIntel || target == BuildTarget.StandaloneOSXIntel64)
+			{
+				CompileNamespacesToFolder(Path.Combine(pathToBuiltProject, "Contents/Resources/Data/Managed"));
+
+			}
+			else if (target == BuildTarget.StandaloneWindows || target == BuildTarget.StandaloneWindows64)
+			{
+				var dataPath = pathToBuiltProject.Replace(".exe", "_Data");
+				CompileNamespacesToFolder(Path.Combine(dataPath, "Managed"));
+
+			}
+			else if (target == BuildTarget.StandaloneLinux || target == BuildTarget.StandaloneLinux64 || target == BuildTarget.StandaloneLinuxUniversal)
+			{
+				var linuxBuildEnding = new Regex("\\..*$");
+				var dataPath = linuxBuildEnding.Replace(pathToBuiltProject, "_Data");
+				CompileNamespacesToFolder(Path.Combine(dataPath, "Managed"));
+
+			}
+			else {
+				EditorUtility.DisplayDialog(
+				  "Unsupported Export Target", "Arcadia does not yet support export to the target " + target, "OK");
+			}
+		}
 	}
-    
-    [PostProcessBuildAttribute(1)]
-    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
-     buildProgress = 0;
-     if(target == BuildTarget.StandaloneOSXUniversal || target == BuildTarget.StandaloneOSXIntel || target == BuildTarget.StandaloneOSXIntel64) {
-		CompileNamespacesToFolder(Path.Combine(pathToBuiltProject, "Contents/Resources/Data/Managed"));
-       
-       } else if(target == BuildTarget.StandaloneWindows || target == BuildTarget.StandaloneWindows64) {
-		var dataPath = pathToBuiltProject.Replace(".exe", "_Data");
-		CompileNamespacesToFolder(Path.Combine(dataPath, "Managed"));
-
-	   } else if (target == BuildTarget.StandaloneLinux || target == BuildTarget.StandaloneLinux64 || target == BuildTarget.StandaloneLinuxUniversal) {
-		var linuxBuildEnding = new Regex("\\..*$");
-		var dataPath = linuxBuildEnding.Replace(pathToBuiltProject, "_Data");
-		CompileNamespacesToFolder(Path.Combine(dataPath, "Managed"));
-
-		} else {
-         EditorUtility.DisplayDialog(
-           "Unsupported Export Target", "Arcadia does not yet support export to the target " + target, "OK");
-       }
-     }
-   }
- }
+}
--- Editor/CustomEditorInspectors.cs
+++ Editor/CustomEditorInspectors.cs
@@ -6,84 +6,94 @@ using UnityEngine;
 using clojure.lang;
 
 [CustomEditor(typeof(ArcadiaBehaviour), true)]
-public class ArcadiaBehaviourEditor : Editor {  
-  public static IFn requireFn;
-  public static IFn intoArrayFn;
-  public static IFn allLoadedUserNamespacesFn;
-  public static IFn titleCase;
-  public static Symbol editorInteropSymbol;
-  
-  static ArcadiaBehaviourEditor() {
-    requireFn = RT.var("clojure.core", "require");
-    requireFn.invoke(Symbol.intern("arcadia.internal.editor-interop"));
-    intoArrayFn = RT.var("clojure.core", "into-array");
-    allLoadedUserNamespacesFn = RT.var("arcadia.internal.editor-interop", "all-loaded-user-namespaces");
-  }
-  
-  void PopupInspector() {
-    EditorGUILayout.Space();
-    ArcadiaBehaviour ab = (ArcadiaBehaviour)target;
-    if(ab.serializedVar != null) {
-      // var, show popup
-      var loadedNamespaces = allLoadedUserNamespacesFn.invoke();
-      Namespace[] namespaces = (Namespace[])intoArrayFn.invoke(loadedNamespaces);
-      var fullyQualifiedVars = namespaces.
-      SelectMany(ns => ns.getMappings().
-        Select((IMapEntry me) => me.val()).
-        Where(v => v.GetType() == typeof(Var) &&
-         ((Var)v).Namespace == ns).
-        Select(v => v.ToString().Substring(2)));
-      
-      string[] blank = new string[] { "" };
-      string[] popUpItems = blank.Concat(fullyQualifiedVars).ToArray();
-      
-      int selectedVar = Array.IndexOf(popUpItems, ab.serializedVar);
-      if(selectedVar < 0) selectedVar = 0;
-      selectedVar = EditorGUILayout.Popup("Function", selectedVar, popUpItems);
-      
-      ab.serializedVar = popUpItems[selectedVar];
-      ab.OnAfterDeserialize();
-    } else {
-      EditorGUILayout.LabelField("Function", ab.fn == null ? "nil" : ab.fn.ToString());
-      
-    }
-  }
-  
-  void TextInspector() {
-    EditorGUILayout.Space();
-    ArcadiaBehaviour ab = (ArcadiaBehaviour)target;
-    ab.serializedVar = EditorGUILayout.TextField("Function", ab.serializedVar);
-    ab.OnAfterDeserialize();
-  }
-  
-  public override void OnInspectorGUI () {
-    // var inspectorConfig = ClojureConfiguration.Get("editor", "hooks-inspector");
-    
-    PopupInspector();
-    /*
-    if(inspectorConfig == Keyword.intern(null, "popup")) {
-      PopupInspector();
-    } else if(inspectorConfig == Keyword.intern(null, "text")) {
-      TextInspector();
-    } else {
-      EditorGUILayout.HelpBox("Invalid value for :editor/hooks-inspector in configuration file." +
-                              "Expected :text or :drop-down, got " + inspectorConfig.ToString() +
-                              ". Showing text inspector.", MessageType.Warning);
-      TextInspector();
-    }
-    */
-  }
+public class ArcadiaBehaviourEditor : Editor
+{
+	public static IFn requireFn;
+	public static IFn intoArrayFn;
+	public static IFn allLoadedUserNamespacesFn;
+	public static IFn titleCase;
+	public static Symbol editorInteropSymbol;
+
+	static ArcadiaBehaviourEditor()
+	{
+		requireFn = RT.var("clojure.core", "require");
+		requireFn.invoke(Symbol.intern("arcadia.internal.editor-interop"));
+		intoArrayFn = RT.var("clojure.core", "into-array");
+		allLoadedUserNamespacesFn = RT.var("arcadia.internal.editor-interop", "all-loaded-user-namespaces");
+	}
+
+	void PopupInspector()
+	{
+		EditorGUILayout.Space();
+		ArcadiaBehaviour ab = (ArcadiaBehaviour)target;
+		if (ab.serializedVar != null)
+		{
+			// var, show popup
+			var loadedNamespaces = allLoadedUserNamespacesFn.invoke();
+			Namespace[] namespaces = (Namespace[])intoArrayFn.invoke(loadedNamespaces);
+			var fullyQualifiedVars = namespaces.
+			SelectMany(ns => ns.getMappings().
+			  Select((IMapEntry me) => me.val()).
+			  Where(v => v.GetType() == typeof(Var) &&
+			   ((Var)v).Namespace == ns).
+			  Select(v => v.ToString().Substring(2)));
+
+			string[] blank = new string[] { "" };
+			string[] popUpItems = blank.Concat(fullyQualifiedVars).ToArray();
+
+			int selectedVar = Array.IndexOf(popUpItems, ab.serializedVar);
+			if (selectedVar < 0) selectedVar = 0;
+			selectedVar = EditorGUILayout.Popup("Function", selectedVar, popUpItems);
+
+			ab.serializedVar = popUpItems[selectedVar];
+			ab.OnAfterDeserialize();
+		}
+		else {
+			EditorGUILayout.LabelField("Function", ab.fn == null ? "nil" : ab.fn.ToString());
+
+		}
+	}
+
+	void TextInspector()
+	{
+		EditorGUILayout.Space();
+		ArcadiaBehaviour ab = (ArcadiaBehaviour)target;
+		ab.serializedVar = EditorGUILayout.TextField("Function", ab.serializedVar);
+		ab.OnAfterDeserialize();
+	}
+
+	public override void OnInspectorGUI()
+	{
+		// var inspectorConfig = ClojureConfiguration.Get("editor", "hooks-inspector");
+
+		PopupInspector();
+		/*
+		if(inspectorConfig == Keyword.intern(null, "popup")) {
+		  PopupInspector();
+		} else if(inspectorConfig == Keyword.intern(null, "text")) {
+		  TextInspector();
+		} else {
+		  EditorGUILayout.HelpBox("Invalid value for :editor/hooks-inspector in configuration file." +
+								  "Expected :text or :drop-down, got " + inspectorConfig.ToString() +
+								  ". Showing text inspector.", MessageType.Warning);
+		  TextInspector();
+		}
+		*/
+	}
 }
 
 [CustomEditor(typeof(ArcadiaState), true)]
-public class ArcadiaStateEditor : Editor {
-  static ArcadiaStateEditor() {
-    RT.load("arcadia/core");
-  }
-  
-  public override void OnInspectorGUI () {
-    ArcadiaBehaviourEditor.requireFn.invoke(Symbol.intern("arcadia.internal.editor-interop"));
-    ArcadiaState stateComponent = (ArcadiaState)target;
-    RT.var("arcadia.internal.editor-interop", "state-inspector!").invoke(stateComponent.state);
-  }
+public class ArcadiaStateEditor : Editor
+{
+	static ArcadiaStateEditor()
+	{
+		RT.load("arcadia/core");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		ArcadiaBehaviourEditor.requireFn.invoke(Symbol.intern("arcadia.internal.editor-interop"));
+		ArcadiaState stateComponent = (ArcadiaState)target;
+		RT.var("arcadia.internal.editor-interop", "state-inspector!").invoke(stateComponent.state);
+	}
 }
