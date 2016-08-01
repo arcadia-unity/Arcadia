98ba18873d476d5621e7c5860772c88a1865dbc4
timsgardner <timsgardner@gmail.com>
2016-08-01 19:20:45 -0400

    adding asset watcher

---
+++ Source/arcadia/internal/asset_watcher.clj
@@ -0,0 +1,23 @@
+(ns arcadia.internal.asset-watcher
+  (:require [arcadia.internal.filewatcher :as fw]
+            [arcadia.internal.file-system :as fs])
+  (:import [System.IO FileSystemInfo]))
+
+;; Contains the arcadia asset filewatcher. Separate namespace to
+;; decouple from arcadia.internal.state (which just holds the
+;; application state atom, ideally containing more or less pure data) and
+;; arcadia.internal.filewatcher itself, which is a general-purpose
+;; filewatcher and shouldn't contain things this specific to Arcadia.
+
+(def ^:private interval 500)
+
+(defonce asset-watcher-ref
+  (atom
+    (fw/start-watch
+      (.FullName
+        (FileSystemInfo.
+          (fs/path-combine "." "Assets")))
+      interval)))
+
+(defn asset-watcher []
+  @asset-watcher-ref)
