c987deb7ae4320cdd6a8b4156d6cbeae2ada918f
timsgardner <timsgardner@gmail.com>
2016-08-04 03:10:05 -0400

    starting to restore leiningen logic

--- Source/arcadia/internal/leiningen.clj
+++ Source/arcadia/internal/leiningen.clj
@@ -2,6 +2,7 @@
   (:require [arcadia.internal.filewatcher :as fw]
             [arcadia.internal.asset-watcher :as aw]
             [arcadia.internal.state :as state]
+            [arcadia.internal.file-system :as fs]
             [clojure.spec :as s]
             [arcadia.internal.spec :as as]))
 
@@ -38,6 +39,95 @@
 
 (defn- compute-raw-dependencies [data]
   (into [] (comp (map :defproject) (map :dependencies) cat) data))
+
+;; ============================================================
+
+(defn- detect-leiningen-projects? []
+  ;; for now:
+  true
+  ;; (boolean
+  ;;   (:detect-leiningen-projects @configuration))
+  )
+
+(defn- ensure-readable-project-file [file-name, raw-file]
+  (let [stringless (clojure.string/replace
+                     raw-file
+                     #"(\".*?((\\\\+)|[^\\])\")|\"\"" ;; fancy string matcher
+                     "")
+        problem (cond
+                  (re-find #"~" stringless)
+                  "'~' found"
+
+                  (re-find #"#" stringless)
+                  "'#' found")]
+    (when problem
+      (throw
+        (Exception.
+          (str "Unsupported file "
+            (.FullName (fs/info file-name))
+            ": " problem))))))
+
+;; stupid for now, expand to deal with exclusions etc
+(defn- normalize-coordinate [coord]
+  (vec
+    (take 3
+      (map str
+        (if (< (count coord) 3)
+          (cons (first coord) coord)
+          coord)))))
+
+(defn- process-coordinates [coords]
+  (->> coords
+    (map normalize-coordinate)
+    (remove #(= ["org.clojure/clojure" "org.clojure/clojure"]
+               (take 2 %)))
+    vec))
+
+(defn- read-lein-project-file [file]
+  (let [raw (slurp file)]
+    (ensure-readable-project-file file raw)
+    (read-string raw)))
+
+(defn- load-leiningen-configuration-map [file]
+  (let [[_ name version & rst] (read-lein-project-file file)
+        m (apply hash-map rst)]
+    (-> m 
+      (select-keys [:dependencies :source-paths])
+      (assoc
+        :type :leiningen
+        :path (.FullName (fs/info file)),
+        :name (str name),
+        :version version,)
+      (update :dependencies process-coordinates))))
+
+(defn- leiningen-project-file? [fi]
+  (= "project.clj" (.Name (fs/info fi))))
+
+(defn- leiningen-structured-directory? [^DirectoryInfo di]
+  (boolean
+    (some leiningen-project-file?
+      (.GetFiles di))))
+
+(defn- leiningen-project-directories []
+  (->> (.GetDirectories (DirectoryInfo. "Assets"))
+    (filter leiningen-structured-directory?)
+    vec))
+
+(defn- leiningen-project-files []
+  (vec
+    (for [^DirectoryInfo di (leiningen-project-directories)
+          ^FileInfo fi (.GetFiles di)
+          :when (leiningen-project-file? fi)]
+      fi)))
+
+;; (defn- leiningen-loadpaths []
+;;   (let [config @configuration]
+;;     (for [m (:config-maps config)
+;;           :when (= :leiningen (:type m))
+;;           :let [p (Path/GetDirectoryName (.FullName (io/as-file (:path m))))]
+;;           sp (or (:source-paths m) ["src" "test"])]
+;;       (combine-paths p sp))))
+
 
 ;; ============================================================
 ;; public facing
@@ -80,3 +170,15 @@ Elements of the returned vector have the following structure:
 ;;  (fn [{:keys [::fw/time ::fw/path]}]
 ;;    ;; stuff happens here
 ;;    ))
+
+
+(comment
+  ((::fw/add-listener (aw/asset-watcher))
+   ::fw/create-modify-delete-file
+   ::config-reload
+   (fn [{:keys [::fw/path]}]
+     (when (and
+             (leiningen-project-file? path)
+             (let [[_ parent grandparent] (fs/path-supers (fs/path path))]
+               (leiningen-structured-directory? (info parent)) ;; this is stupid right now
+               (= )))))))
