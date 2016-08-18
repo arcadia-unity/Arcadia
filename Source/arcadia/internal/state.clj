bd80fc25583b3b95970ad2f4faed4cd83ccef3a8
timsgardner <timsgardner@gmail.com>
2016-08-18 17:52:03 -0400

    adding listener api to state

--- Source/arcadia/internal/state.clj
+++ Source/arcadia/internal/state.clj
@@ -1,4 +1,6 @@
-(ns arcadia.internal.state)
+(ns arcadia.internal.state
+  (:require [clojure.spec :as s]
+            [arcadia.internal.map-utils :as mu]))
 
 ;; ============================================================
 ;; logging
@@ -38,3 +40,45 @@
 (defn updater [kw]
   (fn [f & args]
     (apply swap! state update kw f args)))
+
+;; ==================================================
+;; listeners
+
+(s/fdef add-listener
+  :args (s/cat
+          :listener-group-key qualified-keyword?
+          :listener-key qualified-keyword?
+          :listener ifn?))
+
+(defn add-listener [listener-group-key listener-key listener]
+  (swap! state assoc-in [listener-group-key listener-key] listener))
+
+(defn remove-listener
+  ([listener-group-key]
+   (swap! state dissoc listener-group-key))
+  ([listener-group-key listener-key]
+   (swap! state mu/dissoc-in [listener-group-key listener-key])))
+
+(defn run-listeners
+  ([listener-group-key]
+   (reduce (fn [_ listener]
+             (listener)
+             nil)
+     nil
+     (mu/valsr (get @state listener-group-key))))
+  ([listener-group-key data]
+   (reduce (fn [_ listener]
+             (listener data)
+             nil)
+     nil
+     (mu/valsr (get @state listener-group-key))))
+  ([listener-group-key data & args]
+   (let [args2 (cons data args)]
+     (reduce (fn [_ listener]
+               (apply listener args2)
+               nil)
+       nil
+       (mu/valsr (get @state listener-group-key))))))
+
+(defn listeners [listener-group-key]
+  (get @state listener-group-key))
