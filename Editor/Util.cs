523a5ef2ddc6e8dc8ed2e191437473acdb7929be
Joseph Parker <joseph.parker.1@gmail.com>
2017-02-13 16:22:08 -0500

    fixes #194 by marking open scenes as dirty after every repl eval

---
+++ Editor/Util.cs
@@ -0,0 +1,23 @@
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+namespace Arcadia
+{
+	public class Util
+	{
+		#if UNITY_EDITOR
+		public static bool editor = true;
+		#else
+		public static bool editor = true;
+		#endif
+		
+		public static void MarkScenesDirty(){
+			#if UNITY_EDITOR
+			EditorSceneManager.MarkAllScenesDirty();
+			#endif
+		}
+	}
+}
