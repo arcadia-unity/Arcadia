9a61c79b6a2e3b82d8861acaa80122e7892ff336
Joseph Parker <joseph.parker.1@gmail.com>
2017-02-14 15:59:50 -0500

    move Editor/Util.cs to Helpers/Util.cs

--- Helpers/Util.cs
+++ Helpers/Util.cs
@@ -8,12 +8,6 @@ namespace Arcadia
 {
 	public class Util
 	{
-		#if UNITY_EDITOR
-		public static bool editor = true;
-		#else
-		public static bool editor = false;
-		#endif
-
 		public static void MarkScenesDirty(){
 			#if UNITY_EDITOR
 			EditorSceneManager.MarkAllScenesDirty();
