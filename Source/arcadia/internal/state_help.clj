585278e66d1d86477116158f61ae23a53f1d048b
timsgardner <timsgardner@gmail.com>
2017-04-17 22:12:47 -0400

    bucket o' literals a little more respectable

--- Source/arcadia/internal/state_help.clj
+++ Source/arcadia/internal/state_help.clj
@@ -3,12 +3,17 @@
   (:import [UnityEngine Debug]
            ArcadiaState))
 
+(defn better-merge [m1 m2]
+  (persistent!
+    (reduce-kv assoc! (transient m1) m2)))
+
 (defn awake [^ArcadiaState as]
   (let [state (.state as)]
     (.BuildDatabaseAtom as true)
     (let [objdb (.objectDatabase as)]
       (binding [arcadia.literals/*object-db* objdb
-                *data-readers* arcadia.literals/the-bucket]
+                *data-readers* (better-merge *data-readers*
+ arcadia.literals/the-bucket)]
         (try
           ;; new atom ensures clones made via .instantiate don't share the same atom
           (set! (.state as)
