ef46151059ff7364c65858ff2467b4c41315eef9
timsgardner <timsgardner@gmail.com>
2017-05-03 16:31:30 -0400

    better support for editor callbacks on an interval

--- Editor/EditorCallbacks.cs
+++ Editor/EditorCallbacks.cs
@@ -5,8 +5,28 @@ using clojure.lang;
 
 namespace Arcadia
 {
-	public static class EditorCallbacks
+	public class EditorCallbacks
 	{
+				
+		public class IntervalData
+		{
+			// the function
+			public IFn f;
+			// interval in milliseconds
+			public int interval;
+			
+			public DateTime lastTrigger;
+
+			public object key;
+
+			public IntervalData (IFn _f, int _interval, object _key)
+			{
+				f = _f;
+				interval = _interval;
+				key = _key;
+			}
+		}
+
 		public static bool initialized = false;
 
 		public static Var callbackRunnerVar;
@@ -21,6 +41,22 @@ namespace Arcadia
 
 		public static void RunCallbacks() {
 			callbackRunnerVar.invoke();
+		}
+
+		// I know this looks corny
+		public static void RunIntervalCallbacks(System.Object[] callbacks, IFn onError) {
+			var now = DateTime.Now;
+			for (int i = 0; i < callbacks.Length; i++) {
+				IntervalData d = (IntervalData)callbacks[i];
+				if (d.lastTrigger == null || (now - d.lastTrigger).TotalMilliseconds >= d.interval) {
+					d.lastTrigger = now;
+					try {
+						d.f.invoke();
+					} catch (Exception e) {
+						onError.invoke(e, d);
+					}
+				}
+			}
 		}
 	}
 }
--- Source/arcadia/internal/editor_callbacks.clj
+++ Source/arcadia/internal/editor_callbacks.clj
@@ -1,38 +1,21 @@
 (ns arcadia.internal.editor-callbacks
-  (:require [arcadia.internal.map-utils :as mu])
+  (:require [arcadia.internal.map-utils :as mu]
+            [arcadia.internal.editor-callbacks.types :as ect])
   (:import [UnityEngine Debug]
-           [System.Collections Queue]))
+           [System.Collections Queue]
+           [Arcadia EditorCallbacks EditorCallbacks+IntervalData]
+           [arcadia.internal.editor_callbacks.types RepeatingCallbacks]))
+
+;; ============================================================
+;; normal callbacks
 
 ;; Queue of functions, each of which will be run and then removed from
 ;; the queue in order every time run-callbacks is called.
 (defonce ^Queue work-queue
   (Queue/Synchronized (Queue.)))
 
-;; Map from keys to functions. Each function will be run every time
-;; run-callbacks is called. If a callback throws an exception it will
-;; be removed from repeating-callbacks by dissoc-ing its key.
-(defonce repeating-callbacks
-  (atom {}))
-
 (defn add-callback [f]
   (.Enqueue work-queue f))
-
-(defn add-repeating-callback [k f]
-  (swap! repeating-callbacks assoc k f))
-
-(defn remove-repeating-callback [k]
-  (swap! repeating-callbacks dissoc k))
-
-(defn repeating-callback-runner [_ k f]
-  (try
-    (f)
-    (catch Exception e
-      (Debug/Log
-        (str "Exception encountered when running editor callback at key " k ":"))
-      (Debug/Log e)
-      (Debug/Log (str "Removing editor callback at key " k))
-      (remove-repeating-callback k)))
-  nil)
 
 (defn safe-dequeue-all [^Queue queue]
   (locking queue
@@ -40,6 +23,30 @@
       (let [objs (.ToArray queue)]
         (.Clear queue)
         objs))))
+
+;; ============================================================
+;; repeating callbacks
+
+(defonce repeating-callbacks
+  (atom (ect/build-repeating-callbacks {})))
+
+(defn add-repeating-callback [k f interval]
+  (swap! repeating-callbacks
+    ect/add-repeating-callback k f interval))
+
+(defn remove-repeating-callback [k]
+  (swap! repeating-callbacks ect/remove-repeating-callback k))
+
+(defn handle-repeating-callback-error [^System.Exception e, ^EditorCallbacks+IntervalData ecd]
+  (Debug/LogError e)
+  (Debug/Log "Removing callback at key " (.key ecd))
+  (swap! repeating-callbacks remove-repeating-callback (.key ecd)))
+
+(defn run-repeating-callbacks [^RepeatingCallbacks rcs]
+  (EditorCallbacks/RunIntervalCallbacks (.arr rcs), handle-repeating-callback-error))
+
+;; ============================================================
+;; run all
 
 ;; Gets run by EditorCallbacks.cs on the main thread
 (defn run-callbacks []
@@ -51,5 +58,4 @@
         (Debug/Log
           "Exception encountered when running editor callback")
         (Debug/Log e))))
-  ;; run all the repeating callbacks once
-  (reduce-kv repeating-callback-runner nil @repeating-callbacks))
+  (run-repeating-callbacks @repeating-callbacks))
