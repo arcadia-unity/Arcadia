f8738c82e42295c69b3b69b6019c610565a6f5b5
timsgardner <timsgardner@gmail.com>
2017-06-29 20:26:20 -0400

    reformatting ArcadiaState indents

--- Components/ArcadiaState.cs
+++ Components/ArcadiaState.cs
@@ -4,40 +4,42 @@ using clojure.lang;
 
 public class ArcadiaState : MonoBehaviour, ISerializationCallbackReceiver
 {
-  // TODO sorted maps?
-  public string edn = "{}";
-  public Atom state = new Atom(PersistentHashMap.EMPTY);
-  
-  public Atom objectDatabase = null;
-  public int[] objectDatabaseIds = new int[0];
-  public Object[] objectDatabaseObjects = new Object[0];
-  
-  private static IFn prStr = null;
-  private static IFn readString = null;
-  private static IFn requireFn = null;
-  
-  // creates objectDatabase atom from
-  // objectDatabaseIds and objectDatabaseObjects
-  public void BuildDatabaseAtom(bool force=false) {
-    if(objectDatabase == null || force) {
-      var idsToObjectsMap = PersistentHashMap.EMPTY;
-      
-      if(objectDatabaseIds.Length > 0 && objectDatabaseObjects.Length > 0) {
-        // TODO transients?
-        int len = System.Math.Min(objectDatabaseIds.Length, objectDatabaseObjects.Length);
-        for(int i=0; i<len; i++) {
-          idsToObjectsMap = (PersistentHashMap)idsToObjectsMap.assoc(objectDatabaseIds[i], objectDatabaseObjects[i]);
-        }
-      }
-      
-      objectDatabase = new Atom(idsToObjectsMap);
-    }
-  }
-  
-  void WipeDatabase() {
-    objectDatabase = new Atom(PersistentHashMap.EMPTY);
-  }
-  
+	// TODO sorted maps?
+	public string edn = "{}";
+	public Atom state = new Atom(PersistentHashMap.EMPTY);
+
+	public Atom objectDatabase = null;
+	public int[] objectDatabaseIds = new int[0];
+	public Object[] objectDatabaseObjects = new Object[0];
+
+	private static IFn prStr = null;
+	private static IFn readString = null;
+	private static IFn requireFn = null;
+
+	// creates objectDatabase atom from
+	// objectDatabaseIds and objectDatabaseObjects
+	public void BuildDatabaseAtom (bool force = false)
+	{
+		if (objectDatabase == null || force) {
+			var idsToObjectsMap = PersistentHashMap.EMPTY;
+
+			if (objectDatabaseIds.Length > 0 && objectDatabaseObjects.Length > 0) {
+				// TODO transients?
+				int len = System.Math.Min(objectDatabaseIds.Length, objectDatabaseObjects.Length);
+				for (int i = 0; i < len; i++) {
+					idsToObjectsMap = (PersistentHashMap)idsToObjectsMap.assoc(objectDatabaseIds[i], objectDatabaseObjects[i]);
+				}
+			}
+
+			objectDatabase = new Atom(idsToObjectsMap);
+		}
+	}
+
+	void WipeDatabase ()
+	{
+		objectDatabase = new Atom(PersistentHashMap.EMPTY);
+	}
+
 	private static Var dataReaders;
 
 	private static Var awakeFn;
@@ -69,32 +71,32 @@ public class ArcadiaState : MonoBehaviour, ISerializationCallbackReceiver
 	}
 
 
-	public void OnBeforeSerialize()
-  {
-    if(prStr == null) prStr = (IFn)RT.var("clojure.core", "pr-str");
-    if(requireFn == null) requireFn = (IFn)RT.var("clojure.core", "require");
-    requireFn.invoke(Symbol.intern("arcadia.literals"));
-    Namespace ArcadiaLiteralsNamespace = Namespace.findOrCreate(Symbol.intern("arcadia.literals"));
-    Var ObjectDbVar = Var.intern(ArcadiaLiteralsNamespace, Symbol.intern("*object-db*")).setDynamic();
+	public void OnBeforeSerialize ()
+	{
+		if (prStr == null) prStr = (IFn)RT.var("clojure.core", "pr-str");
+		if (requireFn == null) requireFn = (IFn)RT.var("clojure.core", "require");
+		requireFn.invoke(Symbol.intern("arcadia.literals"));
+		Namespace ArcadiaLiteralsNamespace = Namespace.findOrCreate(Symbol.intern("arcadia.literals"));
+		Var ObjectDbVar = Var.intern(ArcadiaLiteralsNamespace, Symbol.intern("*object-db*")).setDynamic();
 
 		initializeVars();
 
-    WipeDatabase();
-    Var.pushThreadBindings(RT.map(ObjectDbVar, objectDatabase));
-    try {
-      edn = (string)prStr.invoke(state.deref()); // side effects, updating objectDatabase
-      var map = (PersistentHashMap)objectDatabase.deref();
-      objectDatabaseIds  = (int[])RT.seqToTypedArray(typeof(int), RT.keys(map));
-      objectDatabaseObjects = (Object[])RT.seqToTypedArray(typeof(Object), RT.vals(map));
-    } finally {
-      Var.popThreadBindings();
-    }
-  }
-  
-  public void OnAfterDeserialize()
-  {
-#if UNITY_EDITOR  
-    Awake();
+		WipeDatabase();
+		Var.pushThreadBindings(RT.map(ObjectDbVar, objectDatabase));
+		try {
+			edn = (string)prStr.invoke(state.deref()); // side effects, updating objectDatabase
+			var map = (PersistentHashMap)objectDatabase.deref();
+			objectDatabaseIds = (int[])RT.seqToTypedArray(typeof(int), RT.keys(map));
+			objectDatabaseObjects = (Object[])RT.seqToTypedArray(typeof(Object), RT.vals(map));
+		} finally {
+			Var.popThreadBindings();
+		}
+	}
+
+	public void OnAfterDeserialize ()
+	{
+#if UNITY_EDITOR
+		Awake();
 #endif
-  }
+	}
 }
