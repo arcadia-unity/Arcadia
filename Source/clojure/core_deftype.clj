0c2245f45f6f9525a5ed22909122d1eb70f492f5
timsgardner <timsgardner@gmail.com>
2017-07-19 23:45:25 -0400

    fixing deftype doc bug

--- Source/clojure/core_deftype.clj
+++ Source/clojure/core_deftype.clj
@@ -442,23 +442,23 @@
   "(deftype name [fields*]  options* specs*)
   
   Options are expressed as sequential keywords and arguments (in any order).
-
+  
   Supported options:
   :load-ns - if true, importing the record class will cause the
              namespace in which the record was defined to be loaded.
              Defaults to false.
-
+  
   Each spec consists of a protocol or interface name followed by zero
   or more method bodies:
-
+  
   protocol-or-interface-or-Object
   (methodName [args*] body)*
-
+  
   Dynamically generates compiled bytecode for class with the given
   name, in a package with the same name as the current namespace, the
   given fields, and, optionally, methods for protocols and/or
   interfaces. 
-
+  
   The class will have the (by default, immutable) fields named by
   fields, which can have type hints. Protocols/interfaces and methods
   are optional. The only methods that can be supplied are those
@@ -467,21 +467,31 @@
   and those fields can be accessed directly. Fields can be qualified
   with the metadata :volatile-mutable true or :unsynchronized-mutable
   true, at which point (set! afield aval) will be supported in method
-  bodies. Note well that mutable fields are extremely difficult to use
-  correctly, and are present only to facilitate the building of higher
-  level constructs, such as Clojure's reference types, in Clojure
-  itself. They are for experts only - if the semantics and
-  implications of :volatile-mutable or :unsynchronized-mutable are not
-  immediately apparent to you, you should not be using them.
+  bodies. 
 
+  Mutable fields work like C# public instance fields. Fields marked
+  `:volatile-mutable` correspond to C# fields declared with the
+  `volatile` keyword, and are protected against certain compiler
+  optimizations and caching that could (easily) be invalidated by
+  concurrent access, with disastrous results. More documentation on
+  volatile fields in C# can be found here:
+  https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/volatile
+
+  Fields marked `:unsynchronized-mutable` lack this protection, and are
+  usually unsuitable for datastructures that *might* be accessed from
+  multiple threads. On the other hand `:unsynchronized-mutable` fields
+  may be slightly faster (at the level of nanoseconds, of which there
+  are about 16,666,667 in a 60 fps game frame).
+
+  
   Method definitions take the form:
-
+  
   (methodname [args*] body)
-
+  
   The argument and return types can be hinted on the arg and
   methodname symbols. If not supplied, they will be inferred, so type
   hints should be reserved for disambiguation.
-
+  
   Methods should be supplied for all methods of the desired
   protocol(s) and interface(s). You can also define overrides for
   methods of Object. Note that a parameter must be supplied to
@@ -490,18 +500,18 @@
   interface declarations. Note also that recur calls to the method
   head should *not* pass the target object, it will be supplied
   automatically and can not be substituted.
-
+  
   In the method bodies, the (unqualified) name can be used to name the
   class (for calls to new, instance? etc).
-
+  
   When AOT compiling, generates compiled bytecode for a class with the
   given name (a symbol), prepends the current ns as the package, and
   writes the .class file to the *compile-path* directory.
-
+  
   One constructor will be defined, taking the designated fields.  Note
   that the field names __meta and __extmap are currently reserved and
   should not be used when defining your own types.
-
+  
   Given (deftype TypeName ...), a factory function called ->TypeName
   will be defined, taking positional parameters for the fields"
   {:added "1.2"
