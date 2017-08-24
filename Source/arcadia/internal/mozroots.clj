d0a0384e2924c032432dd07dc16d76e2489cbd4c
Ramsey Nasser <ram@nas.sr>
2017-08-24 14:53:20 -0400

    Update windows paths

--- Source/arcadia/internal/mozroots.clj
+++ Source/arcadia/internal/mozroots.clj
@@ -9,17 +9,26 @@
   arcadia.internal.mozroots
   (:import UnityEditor.EditorApplication
            Arcadia.UnityStatusHelper
-           System.Reflection.BindingFlags))
+           System.Reflection.BindingFlags
+           System.IO.Path))
 
 (def path-to-mozroots
-  (let [editor-root EditorApplication/applicationPath]
-    (cond
-      UnityStatusHelper/IsUnityEditorOsx
-      (str editor-root "/Contents/MonoBleedingEdge/lib/mono/4.5/mozroots.exe")
-      UnityStatusHelper/IsUnityEditorWin
-      (str editor-root "???? /Editor/Data/Mono/bin/mozroots")
-      UnityStatusHelper/IsUnityEditorLinux
-      (str editor-root "../Data/MonoBleedingEdge/lib/mono/4.5/mozroots.exe"))))
+  (cond
+    UnityStatusHelper/IsUnityEditorOsx
+    ;; e.g. "/Applications/Unity/Unity.app"
+    (Path/Combine
+      EditorApplication/applicationPath 
+      "Contents/MonoBleedingEdge/lib/mono/4.5/mozroots.exe")
+    UnityStatusHelper/IsUnityEditorWin
+    ;; e.g. "D:/Programs/Unity-Win/Editor/Unity.exe"
+    (Path/Combine
+      (Path/GetDirectoryName EditorApplication/applicationPath) 
+      "Data\\Mono\\bin\\mozroots")
+    UnityStatusHelper/IsUnityEditorLinux
+    ;; e.g. "/home/selfsame/unity-editor-2017.1.0xf3Linux/Editor/Unity"
+    (Path/Combine
+      (Path/GetDirectoryName EditorApplication/applicationPath)
+      "Data/MonoBleedingEdge/lib/mono/4.5/mozroots.exe")))
 
 (assembly-load-file path-to-mozroots)
 
@@ -32,6 +41,3 @@
                             (enum-or BindingFlags/NonPublic BindingFlags/Static))]
     (.Invoke parse-opts nil (into-array Object [(into-array ["--import" "--sync"])]))
     (.Invoke process nil (into-array Object []))))
-
-;; windows
-;; Program Files/Unity/Editor/Data/Mono/bin/mozroots
