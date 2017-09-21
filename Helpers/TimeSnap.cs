e4eed9d3a95d07cf68f779242ca78f778db71269
timsgardner <timsgardner@gmail.com>
2017-09-20 22:48:45 -0400

    adding TimeSnap

---
+++ Helpers/TimeSnap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Arcadia
+{
+	public class TimeSnap
+	{
+
+		//(->> Arcadia.TimeSnap/GlobalTimeSnaps
+		//	(map #(.time %))
+        //  (partition 2)
+        //  (map (fn[[a b]]
+        //         (.Seconds
+		//	    	(System.DateTime/op_Subtraction b a)))))
+		// ==========================================================
+		// static state
+
+		public static List<TimeSnap> GlobalTimeSnaps;
+
+		// ==========================================================
+		// data
+
+		public readonly DateTime time;
+		public readonly string message;
+
+		public TimeSnap (DateTime time_, string message_)
+		{
+			time = time_;
+			message = message_;
+		}
+
+		// ==========================================================
+		// logging equipment
+
+		public static void snap (string message)
+		{
+			if (GlobalTimeSnaps == null) {
+				GlobalTimeSnaps = new List<TimeSnap>();
+			}
+			GlobalTimeSnaps.Add(new TimeSnap(DateTime.Now, message));
+		}
+	}
+}
