979178bb8a4aa6453583f06e162ecc2c8f283b53
pjago <pedro.m4rtins@gmail.com>
2018-03-07 22:21:20 -0300

    catch up with 2018.1b

---
+++ Components/OnJointBreak2DHook.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using clojure.lang;
+
+public class OnJointBreak2DHook : ArcadiaBehaviour   
+{
+  public void OnJointBreak2D(UnityEngine.Joint2D a)
+  {
+
+    RunFunctions(a);
+
+  }
+}
