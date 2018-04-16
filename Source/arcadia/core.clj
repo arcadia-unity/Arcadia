2fba5ac683e75eda9e09958fc4048e20d8e9dfd9
Pedro Martins <pedro.m4rtins@gmail.com>
2018-04-16 02:55:29 -0300

    meh
    why not

--- Source/arcadia/core.clj
+++ Source/arcadia/core.clj
@@ -368,14 +368,14 @@
 
 (defmacro if-cmpt
   "Execute body of code if `gob` has a component of type `cmpt-type`"
-  [gob [cmpt-name cmpt-type] then & [else]]
+  [gob [cmpt-name cmpt-type] then & else]
   (let [gobsym (gentagged "gob__" 'UnityEngine.GameObject)]
     `(if (obj-nil ~gob)
        (with-gobj [~gobsym ~gob]
          (if-let [~(meta-tag cmpt-name cmpt-type) (cmpt ~gobsym ~cmpt-type)]
            ~then
-           ~else))
-       ~else)))
+           ~@else))
+       ~@else)))
 
 ;; ============================================================
 ;; traversal
